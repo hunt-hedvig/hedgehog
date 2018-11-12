e72be3fc2c98cc0c77aec5e66b8be537be558c37
Alex Belyaev <alex.bel.izh@gmail.com>
2018-11-12 14:32:24 +0400

    Fix scrolling on member chat page

--- src/components/chat/tabs/index.tsx
+++ src/components/chat/tabs/index.tsx
@@ -20,7 +20,7 @@ const TabContainer = styled(Tab.Pane)`
 `
 
 const TabItem = ({ props, TabContent, isChatTab }) => (
-  <TabContainer ischattab={isChatTab}>
+  <TabContainer isChatTab={isChatTab}>
     <TabContent {...props} />
   </TabContainer>
 )
@@ -45,7 +45,7 @@ const memberPagePanes = (props, addMessage, socket) => {
         <TabItem
           TabContent={ChatTab}
           props={{ ...props, addMessage, socket }}
-          ischattab={true}
+          isChatTab={true}
         />
       ),
     },
