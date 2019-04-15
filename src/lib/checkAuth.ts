510e9c682016170b99dad1c180d2209fe0f36ef4
Johan Palmfjord <johan.palmfjord@hedvig.com>
2019-04-15 13:13:21 +0200

    Avoid throwing users out in case of recompilation race condition

--- src/lib/checkAuth.ts
+++ src/lib/checkAuth.ts
@@ -2,6 +2,7 @@ import api from 'api'
 import config from 'api/config'
 import { history } from 'store'
 import { setClient } from '../store/actions/clientActions'
+
 /**
  * Check credentials in app store
  * @param {function} getState
@@ -9,6 +10,21 @@ import { setClient } from '../store/actions/clientActions'
 export function checkLocalAuth(getState) {
   const state = getState()
   return !!state.client.id
+}
+
+const makeLoginRequestWithRetryAttempt = async (loginConfig: any) => {
+  try {
+    return await api(loginConfig)
+  } catch (e) {
+    const isNetworkError = (e.message as string).includes('Network Error')
+    if (process.env.NODE_ENV !== 'development' || !isNetworkError) {
+      throw e
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+    return api(config.login.login)
+  }
 }
 
 /**
@@ -19,7 +35,8 @@ export async function checkApiAuth({ dispatch, getState }) {
     if (checkLocalAuth(getState)) {
       return true
     }
-    const client = await api(config.login.login)
+
+    const client = await makeLoginRequestWithRetryAttempt(config.login.login)
     dispatch(setClient(client.data))
     return true
   } catch (error) {
