17a7f9a45fea73c870b033185a6da0e4921d7372
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-05-15 09:19:45 +0200

    removes 'no files uploaded message' from table and style fixes

--- src/components/chat/tabs/FileTab.tsx
+++ src/components/chat/tabs/FileTab.tsx
@@ -1,6 +1,6 @@
-import gql from "graphql-tag";
-import React from "react";
-import { Query } from "react-apollo";
+import gql from 'graphql-tag'
+import * as React from 'react'
+import { Query } from 'react-apollo'
 import { Table, Image } from 'semantic-ui-react'
 
 const query = gql`
@@ -15,78 +15,62 @@ const query = gql`
     }
   }
 `
-const fileDateSorter = (a, b) => {
-    const aDate = new Date(a.timestamp)
-    const bDate = new Date(b.timestamp)
-  
-    if (aDate > bDate) {
-      return -1
-    }
-    if (bDate > aDate) {
-      return 1
-    }
-    return 0
+const sortFileDate = (a, b) => {
+  const aDate = new Date(a.timestamp)
+  const bDate = new Date(b.timestamp)
+
+  return bDate - aDate
 }
 
-const TableExampleCelled = ( { memberFiles } ) => (
-    <Table celled>
-        <Table.Header>
-            <Table.Row>
-                <Table.HeaderCell>Member File</Table.HeaderCell>
-                <Table.HeaderCell>Time Stamp</Table.HeaderCell>
-                <Table.HeaderCell>File Type</Table.HeaderCell>
-            </Table.Row>
-        </Table.Header>
-        <Table.Body>
-            { memberFiles.length === 0 
-            ? "No files uploaded for this member yet" 
-            : memberFiles.sort(fileDateSorter).map((memberFile) => (
-                <Table.Row>
-                    <Table.Cell>
-                        <Image src={ memberFile.fileUploadUrl } size="medium" />
-                    </Table.Cell>
-                    <Table.Cell>
-                        { memberFile.timestamp }
-                    </Table.Cell>
-                    <Table.Cell>
-                        {  memberFile.mimeType }
-                    </Table.Cell>
-                </Table.Row>
-            ))}
-        </Table.Body>
-    </Table>
+const MemberFileTable = ({ memberFiles }) => (
+  <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Member File</Table.HeaderCell>
+        <Table.HeaderCell>Time Stamp</Table.HeaderCell>
+        <Table.HeaderCell>File Type</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      {[...memberFiles].sort(sortFileDate).map((memberFile) => (
+        <Table.Row key={memberFile.fileUploadUrl}>
+          <Table.Cell>
+            <Image src={memberFile.fileUploadUrl} size="medium" />
+          </Table.Cell>
+          <Table.Cell>{memberFile.timestamp}</Table.Cell>
+          <Table.Cell>{memberFile.mimeType}</Table.Cell>
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
 )
 
 class MemberFile extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { clicked: false }
-        this.variables = {
-            memberId: props.match.params.id
-        }
-    }
+  public render() {
+    return (
+      <Query query={query} variables={{ memberId: this.props.match.params.id }}>
+        {({ loading, error, data }) => {
+          if (error) {
+            return (
+              <div>
+                Error in GraphQl query here.....:{' '}
+                <pre>{JSON.stringify(error, null, 2)}</pre>
+              </div>
+            )
+          }
+          if (loading || !data) {
+            return <div>Loading...</div>
+          }
 
-    public render() {
-        return (
-            <Query query={query} variables={ this.variables }>
-                {({ loading, error, data }) => {
-                    if (error) {
-                        return <div>Error in GraphQl query here.....: <pre>{JSON.stringify(error, null, 2)}</pre></div>
-                    }
-                    if (loading || !data) {
-                        return <div>Loading...</div>
-                    }
-
-                    return ( 
-                        <TableExampleCelled
-                            memberFiles={ data.member.fileUploads } 
-                        /> 
-                    )
-                }}
-            </Query>
-
-        )
-    }
+          return data.member.fileUploads.length === 0 ? (
+            <div>No files uploaded for this member</div>
+          ) : (
+            <MemberFileTable memberFiles={data.member.fileUploads} />
+          )
+        }}
+      </Query>
+    )
+  }
 }
 
-export default MemberFile 
+export default MemberFile
