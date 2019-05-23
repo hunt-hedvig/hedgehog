73193da66f8b0ae9c5f735739a4522c48c3760d9
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2019-05-23 20:35:50 +0200

    adds graphQL query and mutation for getting paymentSchedules and approvingMemberCharge

--- src/lib/intl.ts
+++ src/lib/intl.ts
@@ -1,5 +1,5 @@
 export interface Money {
-  amount: number
+  amount: number | string
   currency: 'SEK' | string
 }
 
@@ -7,6 +7,6 @@ export const formatMoney = (locale: 'sv-SE') => (money: Money) =>
   new Intl.NumberFormat(locale, {
     style: 'currency',
     currency: money.currency,
-  }).format(money.amount)
+  }).format(typeof money.amount === "number" ? money.amount : parseFloat(money.amount))
 
 export const formatMoneySE = formatMoney('sv-SE')
