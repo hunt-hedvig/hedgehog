4b37686a3ec29876b1a51ac1821a5f5cd457d9d6
fabian.waldner@gmail.com <fabian.waldner@gmail.com>
2019-07-22 15:23:10 +0200

    reformat

--- src/components/tickets/ticket/util/datetimepicker.tsx
+++ src/components/tickets/ticket/util/datetimepicker.tsx
@@ -15,33 +15,31 @@ interface IDateTimePicker {
 
 export const DateTimePicker = (props: IDateTimePicker) => {
   return (
-     <>
-        <Label htmlFor={props.datepicker.name}>
-          Set remind notification date:
-        </Label>
-        <br />
-        <input
-          type="date"
-          name={props.datepicker.name}
-          value={props.datepicker.value}
-          onChange={(e) => props.handleChange(e)}
-        />
-        <br />
-        <Label htmlFor={props.timepicker.name}>
-          Set remind notification time:
-        </Label>
-        <br />
-        <Input
-          type="text"
-          maxLength={8}
-          placeholder="hh:mm:zz"
-          onChange={(e) => props.handleChange(e)}
-          value={props.timepicker.value}
-          name={props.timepicker.name}
-          pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}"
-        />
-      </>
-    )
-} 
-
-
+    <>
+      <Label htmlFor={props.datepicker.name}>
+        Set remind notification date:
+      </Label>
+      <br />
+      <input
+        type="date"
+        name={props.datepicker.name}
+        value={props.datepicker.value}
+        onChange={(e) => props.handleChange(e)}
+      />
+      <br />
+      <Label htmlFor={props.timepicker.name}>
+        Set remind notification time:
+      </Label>
+      <br />
+      <Input
+        type="text"
+        maxLength={8}
+        placeholder="hh:mm:zz"
+        onChange={(e) => props.handleChange(e)}
+        value={props.timepicker.value}
+        name={props.timepicker.name}
+        pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}"
+      />
+    </>
+  )
+}
