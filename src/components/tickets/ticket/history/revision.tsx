992132b4be9e52e5f9e6ec4bc12cb4e4c00d9fc5
fabian.waldner@gmail.com <fabian.waldner@gmail.com>
2019-08-08 13:21:51 +0200

    more UI stuff for ticket history

--- src/components/tickets/ticket/history/revision.tsx
+++ src/components/tickets/ticket/history/revision.tsx
@@ -1,55 +1,110 @@
 import React from 'react'
-import { Label, Segment  } from 'semantic-ui-react'
+import { Label, Segment, Grid, Button  } from 'semantic-ui-react'
 import { TicketChangeType } from   '../../../../features/taskmanager/types'
 
 
-export const TicketRevision = (props) => {
-	
-    let ticketDetails 
-
-    switch (props.changeType) {
-       case TicketChangeType.TICKET_CREATED:
-            ticketDetails = (<Segment>Ticket created details </Segment>)
-       break;
-        
-       case TicketChangeType.CHANGED_REMINDER: 
-            ticketDetails = (<Segment>Change reminder details </Segment>)
-
-       break;
-
-       case TicketChangeType.CHANGED_ASSIGNED_TO:
-            ticketDetails = (<Segment>Changed assigned to  details </Segment>)
-
-
-       case  TicketChangeType.CHANGED_DESCRIPTION:
-            ticketDetails = (<Segment>Description changed details </Segment>)
-        break
-      
-       case TicketChangeType.CHANGED_STATUS: 
-            ticketDetails = (<Segment>Change status  details </Segment>)
-       break
-      
-       case TicketChangeType.CHANGED_PRIORITY: 
-            ticketDetails = (<Segment>Change priority details </Segment>)
-       break
-
-       default:
-            ticketDetails = (<Segment>Something went wrong... details </Segment>)
-        break;
+export class TicketRevision extends React.Component<{},{}> {
+	public state = {
+        showDetails: false, 
     }
 
+    private toggleShowDetails = (event) => {
+      event.preventDefault()
+      const newState = !this.state.showDetails
+      this.setState( {showDetails: newState })
+    }
+
+    private renderTicketDetails = (changeType) => {
+        switch (changeType) {
+           case TicketChangeType.TICKET_CREATED:
+                return ( 
+                    <Grid columns={2} celled>
+                    <Grid.Row>
+                        <Grid.Column>Original ticket</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={4}><em>Description</em></Grid.Column>
+                        <Grid.Column>{this.props.description}</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={4}><em>Assigned to:</em></Grid.Column>
+                        <Grid.Column>{this.props.assignedTo}</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={4}><em>Manual priority:</em></Grid.Column>
+                        <Grid.Column>{this.props.manualPriority}</Grid.Column>
+                    </Grid.Row>
+                    </Grid>
+                    )
+           break;
+            
+           case TicketChangeType.CHANGED_REMINDER: 
+                return ( 
+                    <Grid columns={2} celled>
+                    <Grid.Row>
+                        <Grid.Column width={4}><em>Reminder date</em></Grid.Column>
+                        <Grid.Column>{this.props.remindDate}</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={4}><em>Remind time: </em></Grid.Column>
+                        <Grid.Column>{this.props.remindTime}</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={4}><em>Remind message: </em></Grid.Column>
+                        <Grid.Column>{this.props.manualPriority}</Grid.Column>
+                    </Grid.Row>
+                    </Grid>)
+           break;
+
+           case TicketChangeType.CHANGED_ASSIGNED_TO:
+                return(<Segment>Changed assigned to  details </Segment>)
+
+
+           case  TicketChangeType.CHANGED_DESCRIPTION:
+                return (<Segment>Description changed details </Segment>)
+            break
+          
+           case TicketChangeType.CHANGED_STATUS: 
+                return (<Segment>Change status  details </Segment>)
+           break
+          
+           case TicketChangeType.CHANGED_PRIORITY: 
+                return (<Segment>Change priority details </Segment>)
+           break
+
+           default:
+                return (<Segment>Something went wrong... details </Segment>)
+            break;
+        }
+    }
+
+    public render() {
     return (
-		<Segment raised vertical>
-			<Label ribbon>Revision</Label>
+		<Segment.Group raised vertical>
+			<Label ribbon color='blue'>Revision</Label>
+            <Button 
+                compact 
+                size="tiny" 
+                color="vk" 
+                onClick={this.toggleShowDetails}
+                floated="right">
+                {this.state.showDetails ? 'Hide details' : 'Show details' }
+            </Button>
 			<Segment.Group horizontal>
-				<Segment><strong>A revision of type:</strong> {props.changeType} </Segment>
-				<Segment><strong>Changed by:</strong> {props.changedBy} </Segment>
-				<Segment><strong>Changed at:</strong> {props.changedAt}</Segment>
+				<Segment><Label attached='top'>A revision of type:</Label> <p>{this.props.changeType}</p> </Segment>
+				<Segment><Label attached='top'>Changed by:</Label> <p>{this.props.changedBy}</p> </Segment>
+				<Segment><Label attached='top'>Changed at:</Label> <p>{this.props.changedAt}</p></Segment>
 			</Segment.Group >
-	        <Segment.Group>
-                {ticketDetails}
-            </Segment.Group>
-    	</Segment>
+            <Segment>
+                {  
+                 this.state.showDetails ? 
+                 this.renderTicketDetails(this.props.changeType) :
+                 null 
+                 }
+            </Segment>
+
+    	</Segment.Group>
         ) 
+    }
 }
 
