71ddffea4e63b9442893d9051eae8d218c07c9c5
Akash Patel <akash@patl.xyz>
2019-11-21 14:39:41 +0100

    remove poll interval

--- src/components/claims/claim-details/components/inventory/ClaimItemDatabase.tsx
+++ src/components/claims/claim-details/components/inventory/ClaimItemDatabase.tsx
@@ -43,7 +43,6 @@ export class ClaimItemDatabase extends React.Component {
         <Grid item xs={12} sm={12} md={4}>
           <Query
             query={GET_INVENTORY}
-            pollInterval={2500}
             variables={{
               claimId: this.props.claimId,
             }}
