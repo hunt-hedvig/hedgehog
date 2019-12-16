385611d068c7f16e5573fa1726834d1848e12dad
Johan Palmfjord <johan.palmfjord@hedvig.com>
2019-12-16 11:11:37 +0100

    Fix the date issue and make some cleanup on the claim type settings page

---
+++ src/components/claims/claim-details/data.ts
@@ -0,0 +1,68 @@
+import gql from 'graphql-tag'
+import { TYPE_FRAGMENT } from './components/ClaimType'
+
+export const CLAIM_PAGE_QUERY = gql`
+  query ClaimPage($id: ID!) {
+    claim(id: $id) {
+      member {
+        memberId
+        signedOn
+        firstName
+        lastName
+        personalNumber
+        address
+        postalNumber
+        city
+        directDebitStatus {
+          activated
+        }
+        fraudulentStatus
+        sanctionStatus
+        numberFailedCharges {
+          numberFailedCharges
+          lastFailedChargeAt
+        }
+        account {
+          totalBalance
+        }
+      }
+      registrationDate
+      recordingUrl
+      state
+      type {
+        ${TYPE_FRAGMENT}
+      }
+      notes {
+        text
+        date
+      }
+      reserves
+      payments {
+        id
+        amount
+        deductible
+        note
+        timestamp
+        exGratia
+        type
+        #transaction {
+        #  status
+        #}
+        status
+      }
+      events {
+        text
+        date
+      }
+      claimFiles {
+        claimFileId
+        fileUploadUrl
+        uploadedAt
+        category
+        contentType
+      }
+      coveringEmployee
+      __typename
+    }
+  }
+`
