bb44685216945713849afbb0e8886cececf3cfb5
rasmusguterstam <rasmus@hedvig.com>
2020-05-02 17:59:53 +0200

    Add other form elements (purchase price, date of purchase, note, button, custom labels)

--- src/components/claims/claim-details/components/claim-items/index.tsx
+++ src/components/claims/claim-details/components/claim-items/index.tsx
@@ -2,12 +2,17 @@ import {
   Button,
   Fade,
   Grid,
+  InputAdornment,
+  MenuItem,
+  Select,
   TextField,
   Typography,
   withStyles,
 } from '@material-ui/core'
+import TodayIcon from '@material-ui/icons/Today'
 import { Paper } from 'components/shared/Paper'
-import { Money } from 'lib/intl'
+import format from 'date-fns/format'
+import { DatePicker } from 'material-ui-pickers'
 import * as React from 'react'
 import {
   SelectedItemCategory,
@@ -26,7 +31,14 @@ export const ClaimItems: React.FC<{ claimId: string }> = ({ claimId }) => {
     SelectedItemCategory[]
   >([])
 
-  const [purchasePrice, setPurchasePrice] = React.useState<Money | null>(null)
+  const [purchasePrice, setPurchasePrice] = React.useState<string>('')
+  const [dateOfPurchase, setDateOfPurchase] = React.useState<string>('')
+  const [purchasePriceCurrency, setPurchasePriceCurrency] = React.useState<
+    string
+  >('SEK')
+  const [dateOfPurchaseActive, setDateOfPurchaseActive] = React.useState<
+    boolean
+  >(false)
 
   return (
     <Paper>
@@ -36,58 +48,97 @@ export const ClaimItems: React.FC<{ claimId: string }> = ({ claimId }) => {
             <h3>Inventory</h3>
           </div>
 
-          <form onSubmit={(e) => e.preventDefault()}>
-            <Grid
-              container
-              style={{ marginBottom: '-18px', marginTop: '10px' }}
-              spacing={16}
-            >
-              <Grid item xs={6}>
-                <TableHeader>Item</TableHeader>
-              </Grid>
-              <Grid item style={{ width: '14.7%' }}>
-                <Fade in={selectedItemCategories.length !== 0}>
-                  <TableHeader>Purchase price</TableHeader>
-                </Fade>
-              </Grid>
-              <Grid item style={{ width: '14.7%' }}>
-                <TableHeader>Date of purchase</TableHeader>
-              </Grid>
-              <Grid item xs={true}>
-                <TableHeader>Note</TableHeader>
-              </Grid>
+          <Grid
+            container
+            style={{ marginBottom: '-18px', marginTop: '10px' }}
+            spacing={16}
+          >
+            <Grid item xs={6}>
+              <TableHeader>Item</TableHeader>
             </Grid>
-            <Grid container spacing={16}>
-              <Grid item xs={6}>
-                <SelectItemCategories
-                  selectedItemCategories={selectedItemCategories}
-                  setSelectedItemCategories={setSelectedItemCategories}
-                />
-              </Grid>
-              <Grid item style={{ width: '14.7%' }}>
-                <TextField
-                  value={purchasePrice}
-                  onChange={(e) => setPurchasePrice(e.target.value)}
-                  placeholder={'Purchase price'}
-                  fullWidth
-                />
-              </Grid>
-              <Grid item style={{ width: '14.7%' }}>
-                <TextField placeholder={'Date of purchase'} fullWidth />
-              </Grid>
-              <Grid item xs={true}>
-                <TextField placeholder={'Note'} fullWidth />
-              </Grid>
+            <Grid item style={{ width: '16.5%' }}>
+              <Fade in={purchasePrice !== ''}>
+                <TableHeader>Purchase price</TableHeader>
+              </Fade>
             </Grid>
-            <Grid container spacing={16}>
-              <Grid item style={{ width: '79.5%' }} />
-              <Grid item xs={true}>
-                <Button fullWidth variant="contained" color="primary">
-                  Add item
-                </Button>
-              </Grid>
+            <Grid item style={{ width: '13.0%' }}>
+              <Fade in={dateOfPurchase !== '' || dateOfPurchaseActive}>
+                <TableHeader>Purchase date</TableHeader>
+              </Fade>
             </Grid>
-          </form>
+            <Grid item xs={true}>
+              <TableHeader>Note</TableHeader>
+            </Grid>
+          </Grid>
+          <Grid container spacing={16}>
+            <Grid item xs={6}>
+              <SelectItemCategories
+                selectedItemCategories={selectedItemCategories}
+                setSelectedItemCategories={setSelectedItemCategories}
+              />
+            </Grid>
+            <Grid item style={{ width: '16.5%' }}>
+              <TextField
+                placeholder={'Purchase price'}
+                onChange={(e) => setPurchasePrice(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Select
+                        style={{ color: '#888' }}
+                        disableUnderline
+                        value={purchasePriceCurrency}
+                        onChange={(e) =>
+                          setPurchasePriceCurrency(e.target.value)
+                        }
+                      >
+                        <MenuItem value={'SEK'}>SEK</MenuItem>
+                        <MenuItem value={'NOK'}>NOK</MenuItem>
+                      </Select>
+                    </InputAdornment>
+                  ),
+                }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item style={{ width: '13.0%' }}>
+              <DatePicker
+                keyboard
+                format={'yyyy-MM-dd'}
+                fullWidth
+                autoOk
+                onFocus={() => setDateOfPurchaseActive(true)}
+                onBlur={() => setDateOfPurchaseActive(false)}
+                value={dateOfPurchase === '' ? null : dateOfPurchase}
+                onChange={(date: Date) => {
+                  date
+                    ? setDateOfPurchase(format(date, 'yyyy-MM-dd'))
+                    : setDateOfPurchase('')
+                }}
+                placeholder="Purchase date"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <TodayIcon
+                        style={{ fontSize: '1.2rem', color: '#555' }}
+                      />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={true}>
+              <TextField placeholder={'Note'} fullWidth helperText={' '} />
+            </Grid>
+          </Grid>
+          <Grid container spacing={16}>
+            <Grid item style={{ width: '79.5%' }} />
+            <Grid item xs={true}>
+              <Button fullWidth variant="contained" color="primary">
+                Add item
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Paper>
