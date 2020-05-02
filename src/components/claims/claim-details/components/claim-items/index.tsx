c4ef816c11f08246d17684bf8034d732eb13c445
rasmusguterstam <rasmus@hedvig.com>
2020-05-02 15:10:53 +0200

    Add custom table header

--- src/components/claims/claim-details/components/claim-items/index.tsx
+++ src/components/claims/claim-details/components/claim-items/index.tsx
@@ -1,15 +1,32 @@
-import { Button, Grid, TextField } from '@material-ui/core'
+import {
+  Button,
+  Fade,
+  Grid,
+  TextField,
+  Typography,
+  withStyles,
+} from '@material-ui/core'
 import { Paper } from 'components/shared/Paper'
+import { Money } from 'lib/intl'
 import * as React from 'react'
 import {
   SelectedItemCategory,
   SelectItemCategories,
 } from './components/SelectItemCategories'
 
+export const TableHeader = withStyles({
+  root: {
+    fontSize: '0.7rem',
+    color: '#333',
+  },
+})(Typography)
+
 export const ClaimItems: React.FC<{ claimId: string }> = ({ claimId }) => {
   const [selectedItemCategories, setSelectedItemCategories] = React.useState<
     SelectedItemCategory[]
   >([])
+
+  const [purchasePrice, setPurchasePrice] = React.useState<Money | null>(null)
 
   return (
     <Paper>
@@ -20,21 +37,46 @@ export const ClaimItems: React.FC<{ claimId: string }> = ({ claimId }) => {
           </div>
 
           <form onSubmit={(e) => e.preventDefault()}>
+            <Grid
+              container
+              style={{ marginBottom: '-18px', marginTop: '10px' }}
+              spacing={16}
+            >
+              <Grid item xs={6}>
+                <TableHeader>Item</TableHeader>
+              </Grid>
+              <Grid item style={{ width: '14.7%' }}>
+                <Fade in={selectedItemCategories.length !== 0}>
+                  <TableHeader>Purchase price</TableHeader>
+                </Fade>
+              </Grid>
+              <Grid item style={{ width: '14.7%' }}>
+                <TableHeader>Date of purchase</TableHeader>
+              </Grid>
+              <Grid item xs={true}>
+                <TableHeader>Note</TableHeader>
+              </Grid>
+            </Grid>
             <Grid container spacing={16}>
-              <Grid style={{ marginTop: '16px' }} item xs={6}>
+              <Grid item xs={6}>
                 <SelectItemCategories
                   selectedItemCategories={selectedItemCategories}
                   setSelectedItemCategories={setSelectedItemCategories}
                 />
               </Grid>
-              <Grid item xs={'auto'}>
-                <TextField label={'Purchase price'} fullWidth />
+              <Grid item style={{ width: '14.7%' }}>
+                <TextField
+                  value={purchasePrice}
+                  onChange={(e) => setPurchasePrice(e.target.value)}
+                  placeholder={'Purchase price'}
+                  fullWidth
+                />
               </Grid>
-              <Grid item xs={'auto'}>
-                <TextField label={'Date of purchase'} fullWidth />
+              <Grid item style={{ width: '14.7%' }}>
+                <TextField placeholder={'Date of purchase'} fullWidth />
               </Grid>
               <Grid item xs={true}>
-                <TextField label={'Note'} fullWidth />
+                <TextField placeholder={'Note'} fullWidth />
               </Grid>
             </Grid>
             <Grid container spacing={16}>
