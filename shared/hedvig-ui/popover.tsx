0938d19756e7cc8c30ed3d6b6a84100aea01ec7d
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-06-05 15:34:14 +0200

    Add popover ui componentt

---
+++ shared/hedvig-ui/popover.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import styled, { css } from 'react-emotion'
+
+const PopoverWrapper = styled.div`
+  position: relative;
+`
+
+const Contents = styled.div`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, calc(-100% - 0.25rem));
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+
+  ${({ theme }) => css`
+    background: ${theme.foreground};
+    color: ${theme.background};
+  `}
+`
+const Triangle = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translate(-50%, 100%);
+  width: 0;
+  height: 0;
+  border-left: 0.5rem solid transparent;
+  border-right: 0.5rem solid transparent;
+  border-top: 0.5rem solid ${({ theme }) => theme.foreground};
+`
+
+interface PopoverProps {
+  contents: React.ReactNode
+  disable?: boolean
+  onOpen?: () => void
+  onClose?: () => void
+}
+export const Popover: React.FC<PopoverProps> = ({
+  contents,
+  disable,
+  onOpen,
+  onClose,
+  children,
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <PopoverWrapper
+      onMouseOver={() => {
+        setIsOpen(true)
+        if (onOpen) {
+          onOpen()
+        }
+      }}
+      onMouseLeave={() => {
+        setIsOpen(false)
+        if (onClose) {
+          onClose()
+        }
+      }}
+    >
+      {isOpen && !disable && (
+        <Contents>
+          {contents}
+          <Triangle />
+        </Contents>
+      )}
+      {children}
+    </PopoverWrapper>
+  )
+}
