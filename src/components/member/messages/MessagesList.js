63fd8c23293b7fa21e921d107d0bed933171ea34
vonElfvin <elvin.granat@gmail.com>
2020-06-27 13:30:07 +0200

    Another attempt at fixing iOS messages

--- src/components/member/messages/MessagesList.js
+++ src/components/member/messages/MessagesList.js
@@ -17,7 +17,7 @@ const MessagesListContainer = styled('div')(({ theme }) => ({
   height: '100%',
   display: 'flex',
   flexDirection: 'column-reverse',
-  flex: 1
+  flex: '1 1 0%'
 }))
 
 const EmptyList = styled('h3')({
