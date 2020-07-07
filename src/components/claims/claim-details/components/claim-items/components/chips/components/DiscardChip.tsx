88ded6430d9ae13021f4143d405760bf03dfc788
rasmusguterstam <rasmus@hedvig.com>
2020-07-07 19:06:02 +0200

    Bump styling specificity to override the source order

--- src/components/claims/claim-details/components/claim-items/components/chips/components/DiscardChip.tsx
+++ src/components/claims/claim-details/components/claim-items/components/chips/components/DiscardChip.tsx
@@ -4,13 +4,15 @@ import styled from 'react-emotion'
 import { BaseChip } from './BaseChip'
 
 const Chip = styled(BaseChip)`
-  background-color: transparent;
-  color: ${({ theme }) => theme.accent};
-  margin-top: 8px;
-  font-weight: bold;
-  margin-left: 7px;
-  cursor: pointer;
-  border: 1px solid ${({ theme }) => theme.accent};
+  && {
+    background-color: transparent;
+    color: ${({ theme }) => theme.accent};
+    margin-top: 8px;
+    font-weight: bold;
+    margin-left: 7px;
+    cursor: pointer;
+    border: 1px solid ${({ theme }) => theme.accent};
+  }
 `
 
 export const DiscardChip: React.FC<{ onClick: React.EventHandler<any> }> = ({
