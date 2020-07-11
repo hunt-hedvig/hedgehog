89ed51d2fc195e8ae3a84d6669720007a1044ed9
rasmusguterstam <rasmus@hedvig.com>
2020-07-11 17:40:36 +0200

    Redeem / Unredeem structure

--- src/components/member/tabs/referrals-tab/index.tsx
+++ src/components/member/tabs/referrals-tab/index.tsx
@@ -3,12 +3,14 @@ import {
   InfoRow,
   InfoText,
 } from 'components/member/tabs/contracts-tab/contract'
+import { useGetReferralInformation } from 'graphql/use-get-referral-information'
+import { Button, ButtonsGroup } from 'hedvig-ui/button'
 import { Card, CardsWrapper } from 'hedvig-ui/card'
+import { Input } from 'hedvig-ui/input'
 import { MainHeadline } from 'hedvig-ui/typography'
 import React from 'react'
 import styled from 'react-emotion'
 import { Table } from 'semantic-ui-react'
-import { useGetReferralInformation } from 'graphql/use-get-referral-information'
 
 const Headline = styled(MainHeadline)`
   display: flex;
@@ -23,27 +25,102 @@ const ReferralsWrapper = styled('div')`
   }
 `
 
+interface ForeverStatusProps {
+  eligible: boolean
+}
+
+const ForeverStatusBadge = styled('div')<ForeverStatusProps>`
+  padding: 0.5rem 1rem;
+  line-height: 1;
+  background: ${({ eligible, theme }) =>
+    eligible ? theme.success : theme.danger};
+  border-radius: 8px;
+  color: #fff;
+`
+
+const CampaignCodeBadge = styled('div')`
+  padding: 0.5rem 1rem;
+  line-height: 1;
+  background: ${({ theme }) => theme.accent};
+  border-radius: 8px;
+  color: ${({ theme }) => theme.accentContrast};
+  font-weight: bold;
+`
+
 export const ReferralsTab: React.FunctionComponent<{ memberId: string }> = ({
   memberId,
 }) => {
   const [referralInformation] = useGetReferralInformation(memberId)
+  const [referralCode, setReferralCode] = React.useState('')
+
   return (
     <>
-      {console.log(referralInformation)}
       <Headline>Referrals</Headline>
       <ReferralsWrapper>
         <CardsWrapper>
-          <Card span={1}>
+          <Card span={2}>
             <InfoContainer>
-              <InfoRow>
-                Campaign Code
-                <InfoText>{memberId}</InfoText>
+              <InfoRow style={{ marginTop: '0.4rem' }}>
+                <span style={{ marginTop: '0.3rem' }}>Campaign Code</span>
+                <InfoText>
+                  {(
+                    <CampaignCodeBadge>
+                      {referralInformation?.campaign.code.toUpperCase()}
+                    </CampaignCodeBadge>
+                  ) ?? 'Not available'}
+                </InfoText>
               </InfoRow>
-              <InfoRow>
-                Hedvig Forever status
-                <InfoText>ACTIVE</InfoText>
+              <InfoRow style={{ marginTop: '1.5rem' }}>
+                <span style={{ marginTop: '0.3rem' }}>
+                  Hedvig Forever status
+                </span>
+                <InfoText>
+                  {referralInformation ? (
+                    <ForeverStatusBadge eligible={referralInformation.eligible}>
+                      {referralInformation.eligible ? 'Active' : 'Inactive'}
+                    </ForeverStatusBadge>
+                  ) : (
+                    'Not available'
+                  )}
+                </InfoText>
               </InfoRow>
             </InfoContainer>
+          </Card>
+          <Card span={2}>
+            {referralInformation?.eligible ? (
+              <>
+                <Input
+                  placeholder={'Campaign code'}
+                  value={referralCode}
+                  onChange={(_e, { value }) => setReferralCode(value)}
+                />
+                <ButtonsGroup style={{ marginTop: '1.0rem' }}>
+                  <Button
+                    variation="primary"
+                    fullWidth
+                    onClick={() => console.log('Hello world')}
+                  >
+                    Unredeem
+                  </Button>
+                  <Button
+                    variation="primary"
+                    fullWidth
+                    onClick={() => console.log('Hello world')}
+                  >
+                    Redeem
+                  </Button>
+                </ButtonsGroup>
+              </>
+            ) : (
+              <Button
+                variation="primary"
+                fullWidth
+                onClick={() => console.log('Hello world')}
+                style={{ marginTop: '1.0rem' }}
+              >
+                Activate Hedvig Forever
+              </Button>
+            )}
           </Card>
         </CardsWrapper>
       </ReferralsWrapper>
