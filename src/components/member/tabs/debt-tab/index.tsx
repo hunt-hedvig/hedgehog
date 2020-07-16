7649cc20be4d6ac284847689e77a32cae011da00
rasmusguterstam <rasmus@hedvig.com>
2020-07-16 18:03:45 +0200

    Add headline, implement hook useGetPerson

--- src/components/member/tabs/debt-tab/index.tsx
+++ src/components/member/tabs/debt-tab/index.tsx
@@ -9,16 +9,45 @@ import {
   InfoRow,
   InfoText,
 } from 'components/member/tabs/contracts-tab/contract'
+import { useGetPerson } from 'graphql/use-get-person'
 import { Button } from 'hedvig-ui/button'
 import { Card, CardsWrapper } from 'hedvig-ui/card'
 import { OrbIndicator } from 'hedvig-ui/orb-indicator'
 import { Spacing } from 'hedvig-ui/spacing'
-import { ThirdLevelHeadline } from 'hedvig-ui/typography'
+import { MainHeadline, ThirdLevelHeadline } from 'hedvig-ui/typography'
 import { dateTimeFormatter } from 'lib/helpers'
 import * as React from 'react'
-import { Query } from 'react-apollo'
+import { ArrowRepeat } from 'react-bootstrap-icons'
+import styled, { css, keyframes } from 'react-emotion'
 import { Table } from 'semantic-ui-react'
 import { formatMoney } from 'utils/money'
+
+const Headline = styled(MainHeadline)`
+  display: flex;
+  align-items: center;
+`
+
+const spin = keyframes`
+  from{transform: rotate(0deg)}
+  to{transform: rotate(360deg)}
+`
+const RefreshButton = styled.button<{ loading: boolean }>`
+  background: transparent;
+  font-size: 0.875em;
+  color: ${({ theme }) => theme.mutedText};
+  padding: 0;
+  border: 0;
+  margin-left: 1rem;
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  transition: transform 500ms;
+  ${({ loading }) =>
+    loading &&
+    css`
+      animation: ${spin} 500ms linear infinite;
+    `};
+`
 
 const PaymentDefaultsTable: React.FunctionComponent<{
   paymentDefaults: PaymentDefault[]
@@ -105,84 +134,91 @@ const OverallDebtProfile: React.FunctionComponent<{
 )
 
 export const DebtTab: React.FC<{
-  showNotification: (data: any) => void
+  memberId: string
   contractMarketInfo: ContractMarketInfo
-}> = ({ contractMarketInfo }) => {
+}> = ({ memberId, contractMarketInfo }) => {
+  const [person, { loading, error, refetch }] = useGetPerson(memberId)
+
   // FIXME: We should not make market specific features like this, should use "have debt" or "don't have debt" instead
   if (contractMarketInfo?.market === Market.Norway) {
     return <>Not available for Norway</>
   }
 
+  if (loading) {
+    return (
+      <>
+        <Headline>
+          Debt
+          <RefreshButton onClick={() => refetch()} loading={loading}>
+            <ArrowRepeat />
+          </RefreshButton>
+        </Headline>
+        Loading...
+      </>
+    )
+  }
+
+  if (error || !person) {
+    return (
+      <>
+        <Headline>
+          Debt
+          <RefreshButton onClick={() => refetch()} loading={loading}>
+            <ArrowRepeat />
+          </RefreshButton>
+        </Headline>
+        Issue retrieving debt for this member
+      </>
+    )
+  }
+
   return (
-    <Query<any>
-      query={query}
-      variables={{ memberId: this.props.match.params.memberId }}
-    >
-      {({ loading, error, data }) => {
-        if (error) {
-          return <div>Issue retrieving debt for this member</div>
-        }
-        if (loading || !data) {
-          return <div>Loading...</div>
-        }
+    <>
+      <Headline>
+        Debt
+        <RefreshButton onClick={() => refetch()} loading={loading}>
+          <ArrowRepeat />
+        </RefreshButton>
+      </Headline>
+      <CardsWrapper>
+        <Card span={2}>
+          <InfoContainer>
+            <InfoRow>
+              Member flag
+              <InfoText>
+                {person.status.flag && (
+                  <OrbIndicator color={person.status.flag} size={'tiny'} />
+                )}
+              </InfoText>
+            </InfoRow>
+            <InfoRow>
+              Member status
+              <InfoText>
+                {person.status.whitelisted ? 'Whitelisted' : 'Not Whitelisted'}
+              </InfoText>
+            </InfoRow>
+          </InfoContainer>
+          <Spacing top={'large'} />
+          {!person.status.whitelisted &&
+            person.debt.paymentDefaults.length !== 0 && (
+              <Button
+                variation="primary"
+                fullWidth
+                onClick={() => {
+                  const confirm = window.confirm('Are you sure?')
 
-        return (
-          <>
-            {!data.member || !data.member.person ? (
-              'Issue retrieving debt for this member'
-            ) : (
-              <>
-                <CardsWrapper>
-                  <Card span={2}>
-                    <InfoContainer>
-                      <InfoRow>
-                        Member flag
-                        <InfoText>
-                          {data.member.person.status.flag && (
-                            <OrbIndicator
-                              color={data.member.person.status.flag}
-                              size={'tiny'}
-                            />
-                          )}
-                        </InfoText>
-                      </InfoRow>
-                      <InfoRow>
-                        Member status
-                        <InfoText>
-                          {data.member.person.status.whitelisted
-                            ? 'Whitelisted'
-                            : 'Not Whitelisted'}
-                        </InfoText>
-                      </InfoRow>
-                    </InfoContainer>
-                    <Spacing top={'large'} />
-                    {!data.member.person.status.whitelisted &&
-                      data.member.person.debt.paymentDefaults.length !== 0 && (
-                        <Button
-                          variation="primary"
-                          fullWidth
-                          onClick={() => {
-                            const confirm = window.confirm('Are you sure?')
-
-                            if (confirm) {
-                              console.log('AAAAAAAAAAA')
-                            }
-                          }}
-                        >
-                          Whitelist Member
-                        </Button>
-                      )}
-                  </Card>
-                  <OverallDebtProfile debt={data.member.person.debt} />
-                </CardsWrapper>
-                <PaymentDefaultsTable
-                  paymentDefaults={data.member.person.debt.paymentDefaults}
-                />
-              </>
+                  if (confirm) {
+                    console.log('AAAAAAAAAAA')
+                  }
+                }}
+              >
+                Whitelist Member
+              </Button>
             )}
-          </>
-        )
-      }}
-    </Query>
+        </Card>
+        <OverallDebtProfile debt={person.debt} />
+      </CardsWrapper>
+      <PaymentDefaultsTable paymentDefaults={person.debt.paymentDefaults} />
+    </>
   )
 }
