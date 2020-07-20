f86f334ca433df7b04abd700db112a00133421de
rasmusguterstam <rasmus@hedvig.com>
2020-07-20 16:05:26 +0200

    Inject memberId into claims tab

--- src/components/member/tabs/index.js
+++ src/components/member/tabs/index.js
@@ -13,6 +13,7 @@ import { Tab } from 'semantic-ui-react'
 import styled from 'react-emotion'
 import { useContractMarketInfo } from 'graphql/use-get-member-contract-market-info'
 import { AccountTab } from './account-tab'
+import { CampaignsTab } from './campaigns-tab'
 
 const TabContainer = styled(Tab.Pane)`
   &&& {
@@ -42,14 +43,16 @@ TabItem.propTypes = {
 
 const memberPagePanes = (props) => {
   const memberId = props.match.params.memberId
-  const panes = [
+  return [
     {
       menuItem: 'Member',
       render: () => <TabItem props={props} TabContent={DetailsTab} />,
     },
     {
       menuItem: 'Claims',
-      render: () => <TabItem props={{...props, memberId}} TabContent={ClaimsTab} />,
+      render: () => (
+        <TabItem props={{ ...props, memberId }} TabContent={ClaimsTab} />
+      ),
     },
     {
       menuItem: 'Files',
@@ -68,8 +71,6 @@ const memberPagePanes = (props) => {
         />
       ),
     },
-  ]
-  panes.push(
     {
       menuItem: 'Contracts',
       render: () => (
@@ -82,30 +83,26 @@ const memberPagePanes = (props) => {
         <TabItem props={{ ...props, memberId }} TabContent={Quotes} />
       ),
     },
-  )
-  panes.push(
     {
       menuItem: 'Payments',
-      render: () => (
-        <TabItem memberId={memberId} props={props} TabContent={PaymentsTab} />
-      ),
+      render: () => <TabItem props={props} TabContent={PaymentsTab} />,
     },
     {
       menuItem: 'Account',
       render: () => (
-        <TabItem
-          memberId={memberId}
-          props={{ ...props, memberId }}
-          TabContent={AccountTab}
-        />
+        <TabItem props={{ ...props, memberId }} TabContent={AccountTab} />
       ),
     },
     {
       menuItem: 'Debt',
       render: () => <TabItem props={props} TabContent={MemberDebtComponent} />,
     },
-  )
-
-  return panes
+    {
+      menuItem: 'Campaigns',
+      render: () => (
+        <TabItem props={{ ...props, memberId }} TabContent={CampaignsTab} />
+      ),
+    },
+  ]
 }
 export default memberPagePanes
