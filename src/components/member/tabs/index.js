a5867853dc8e2900f0b4336b24728ef16386d79f
rasmusguterstam <rasmus@hedvig.com>
2020-07-23 10:44:14 +0200

    Clean unused props in memberPagePanes

--- src/components/member/tabs/index.js
+++ src/components/member/tabs/index.js
@@ -12,7 +12,7 @@ import { Tab } from 'semantic-ui-react'
 import styled from 'react-emotion'
 import { useContractMarketInfo } from 'graphql/use-get-member-contract-market-info'
 import { AccountTab } from './account-tab'
-import { DebtTab } from 'components/member/tabs/debt-tab'
+import { DebtTab } from './debt-tab'
 
 const TabContainer = styled(Tab.Pane)`
   &&& {
@@ -42,7 +42,7 @@ TabItem.propTypes = {
 
 const memberPagePanes = (props) => {
   const memberId = props.match.params.memberId
-  const panes = [
+  return [
     {
       menuItem: 'Member',
       render: () => <TabItem props={props} TabContent={DetailsTab} />,
@@ -70,8 +70,6 @@ const memberPagePanes = (props) => {
         />
       ),
     },
-  ]
-  panes.push(
     {
       menuItem: 'Contracts',
       render: () => (
@@ -84,19 +82,17 @@ const memberPagePanes = (props) => {
         <TabItem props={{ ...props, memberId }} TabContent={Quotes} />
       ),
     },
-  )
-  panes.push(
+
     {
       menuItem: 'Payments',
       render: () => (
-        <TabItem memberId={memberId} props={props} TabContent={PaymentsTab} />
+        <TabItem props={props} TabContent={PaymentsTab} />
       ),
     },
     {
       menuItem: 'Account',
       render: () => (
         <TabItem
-          memberId={memberId}
           props={{ ...props, memberId }}
           TabContent={AccountTab}
         />
@@ -108,8 +104,6 @@ const memberPagePanes = (props) => {
         <TabItem props={{ ...props, memberId }} TabContent={DebtTab} />
       ),
     },
-  )
-
-  return panes
+  ]
 }
 export default memberPagePanes
