6d88c184bdd92bc5729b2a0de9aca24ec54959c7
Gustaf H <gustaf.halvardsson@gmail.com>
2020-08-03 14:37:06 +0200

    reformat

--- src/components/questions/questions-list/QuestionGroups.js
+++ src/components/questions/questions-list/QuestionGroups.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'react-emotion'
 import { FilteredQuestionGroups } from './FilteredQuestionGroups'
 import { FilterState } from 'components/questions/filter'
-import { isMemberIdEven} from 'utils/member'
+import { isMemberIdEven } from 'utils/member'
 import { useQuestionGroups } from 'graphql/use-question-groups'
 
 const ListContainer = styled.div`
