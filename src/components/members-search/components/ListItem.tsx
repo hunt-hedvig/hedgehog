e3ca55c11c7ffc75a35a3fd2cb5f63e3298e87a1
rasmusguterstam <rasmus@hedvig.com>
2020-09-21 15:23:03 +0200

    Add circles

--- src/components/members-search/components/ListItem.tsx
+++ src/components/members-search/components/ListItem.tsx
@@ -2,9 +2,28 @@ import { Member } from 'api/generated/graphql'
 import { MemberAgeWrapper } from 'components/members-search/styles'
 import { format, parseISO } from 'date-fns'
 import React from 'react'
+import styled from 'react-emotion'
 import { Link } from 'react-router-dom'
 import { Table } from 'semantic-ui-react'
 import { getMemberFlag, MemberAge } from 'utils/member'
+
+const CircleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.accentContrast};
+`
+
+const Circle = styled.div`
+  margin: 0 5px;
+  height: 25px;
+  width: 25px;
+  background-color: ${({ theme }) => theme.success};
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export const ListItem: React.FC<{ member: Member }> = ({ member }) => {
   const market = member?.contractMarketInfo?.market
@@ -30,7 +49,11 @@ export const ListItem: React.FC<{ member: Member }> = ({ member }) => {
           format(parseISO(member.signedOn), 'MMM d, yyy, HH:ii')}
       </Table.Cell>
       <Table.Cell>
-        <Badge></Badge>
+        <CircleWrapper>
+          <Circle>0</Circle>
+          <Circle>0</Circle>
+          <Circle>0</Circle>
+        </CircleWrapper>
       </Table.Cell>
     </Table.Row>
   )
