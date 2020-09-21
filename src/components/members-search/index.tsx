9df1d4c52a65f25d70f52af3a84bb8474abdd4b6
rasmusguterstam <rasmus@hedvig.com>
2020-09-21 12:57:56 +0200

    Extract styles to styles.tsx

--- src/components/members-search/index.tsx
+++ src/components/members-search/index.tsx
@@ -1,68 +1,29 @@
 import { Member } from 'api/generated/graphql'
+import {
+  EscapeButton,
+  ExtraInstruction,
+  Group,
+  Instructions,
+  ListWrapper,
+  MemberAgeWrapper,
+  MemberSuggestionsWrapper,
+  NoMembers,
+  SearchButton,
+  SearchIcon,
+  SearchInput,
+  SearchInputGroup,
+} from 'components/members-search/styles'
 import BackendPaginatorList from 'components/shared/paginator-list/BackendPaginatorList'
 import { format, parseISO } from 'date-fns'
 import { useMemberSearch } from 'graphql/use-member-search'
-import { Button } from 'hedvig-ui/button'
 import { Checkbox } from 'hedvig-ui/checkbox'
-import { Input } from 'hedvig-ui/input'
 import { MainHeadline } from 'hedvig-ui/typography'
 import React from 'react'
-import { Search as SearchBootstrapIcon } from 'react-bootstrap-icons'
-import styled, { keyframes } from 'react-emotion'
 import { Link } from 'react-router-dom'
 import { Table } from 'semantic-ui-react'
 import { InsuranceStatusBadge } from 'utils/agreement'
 import { MemberAge } from 'utils/member'
 import { MemberSuggestions } from './member-suggestions'
-
-const fadeIn = (max: number) =>
-  keyframes({
-    from: { opacity: 0, transform: 'translateY(5%)' },
-    to: { opacity: max, transform: 'translateY(0)' },
-  })
-
-const Instructions = styled('div')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  textAlign: 'left',
-  paddingLeft: '1rem',
-  paddingTop: '2rem',
-  code: {
-    background: theme.backgroundTransparent,
-    padding: '1px 2px',
-    borderRadius: 1,
-  },
-  opacity: 0,
-  animation: `${fadeIn(0.3)} 1000ms forwards`,
-  animationDelay: '500ms',
-}))
-
-const MemberSuggestionsWrapper = styled(Instructions)({
-  paddingTop: '25vh',
-  width: '100%',
-  maxWidth: '50rem',
-  animation: `${fadeIn(1)} 1000ms forwards`,
-  animationDelay: '750ms',
-})
-
-const NoMembers = styled(Instructions)({
-  width: '100%',
-  flex: 1,
-  alignItems: 'center',
-  fontSize: '1.5rem',
-  paddingTop: '25vh',
-})
-
-const ExtraInstruction = styled('div')({
-  opacity: 0,
-  animation: `${fadeIn(1)} 1000ms forwards`,
-  animationDelay: '1000ms',
-})
-
-const ListWrapper = styled('div')({
-  paddingLeft: '1rem',
-})
 
 export const MembersSearch: React.FC = () => {
   const [query, setQuery] = React.useState('')
@@ -143,51 +104,6 @@ export const MembersSearch: React.FC = () => {
     </>
   )
 }
-
-const Group = styled('div')<{ pushLeft?: boolean }>(({ pushLeft }) => ({
-  paddingBottom: '1rem',
-  paddingLeft: pushLeft ? '1rem' : 0,
-}))
-const SearchInputGroup = styled('div')({
-  display: 'flex',
-  position: 'relative',
-  fontSize: '1rem',
-  maxWidth: '40rem',
-})
-const SearchIcon = styled(SearchBootstrapIcon)<{ muted: boolean }>(
-  ({ muted, theme }) => ({
-    position: 'absolute',
-    top: '50%',
-    left: '1rem',
-    transform: 'translateY(-50%)',
-    zIndex: 1,
-    fill: muted ? theme.mutedText : undefined,
-    transition: 'fill 300ms',
-  }),
-)
-
-const SearchInput = styled(Input)({
-  marginRight: '1rem',
-
-  '&&': {
-    width: '100%',
-  },
-
-  '&& input': {
-    borderRadius: '0.5rem',
-    paddingLeft: '3rem',
-  },
-})
-const SearchButton = styled(Button)<{ visible: boolean }>(({ visible }) => ({
-  opacity: visible ? 1 : 0,
-  transition: 'opacity 400ms',
-}))
-
-const EscapeButton = styled(Button)<{ visible: boolean }>(({ visible }) => ({
-  opacity: visible ? 1 : 0,
-  transition: 'opacity 300ms',
-  marginLeft: '2rem',
-}))
 
 const Search: React.FC<{
   onSubmit: (query: string, includeAll: boolean) => void
@@ -270,11 +186,6 @@ const ListHeader: React.FC = () => (
     <Table.HeaderCell>Size</Table.HeaderCell>
   </Table.Header>
 )
-
-const MemberAgeWrapper = styled('div')(({ theme }) => ({
-  color: theme.mutedText,
-  fontSize: '0.8rem',
-}))
 
 const ListItem: React.FC<{ member: Member }> = ({ member }) => {
   // TODO: @Elvin, is productStatus something from ProductPricing? Verify what to resolve
