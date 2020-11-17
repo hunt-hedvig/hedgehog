3ef7c7c46f4162da09c7f80dbd3df51f1cecdb83
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-11-17 11:47:12 +0100

    change log entry

--- src/changelog.ts
+++ src/changelog.ts
@@ -12,6 +12,12 @@ export interface Change {
 
 export const changelog: ReadonlyArray<Change> = [
   {
+    date: '2020-11-17',
+    change:
+      'Unsigne tool now notifies if the member was signed or not on unsigning',
+    authorGithubHandle: 'fredrikareschoug',
+  },
+  {
     date: '2020-10-13',
     change:
       "Use your your keyboard's up and down arrow keys to navigate the member search and claims list",
