68d57ece9b2f978cd10d859550624efdfaa5e066
rasmusguterstam <rasmus@hedvig.com>
2021-03-25 08:32:48 +0100

    Update changelog.ts

--- src/changelog.ts
+++ src/changelog.ts
@@ -12,6 +12,12 @@ export interface Change {
 
 export const changelog: ReadonlyArray<Change> = [
   {
+    date: '2021-03-25',
+    change:
+      'Shortcuts! Hold CTRL to show available shortcuts on a page. Press CTRL and SPACE to open searchable interface. ',
+    authorGithubHandle: 'rasmusguterstam',
+  },
+  {
     date: '2021-03-04',
     change:
       'Add ability to change how many colors are in H.OPE. and other improvements',
