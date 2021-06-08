c31eae864027de291c13e7d9d617e3c3c427e214
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2021-06-08 17:14:31 +0200

    don't allow gaps in insurnace

--- src/components/member/tabs/quote-tab/quote-sub-section.tsx
+++ src/components/member/tabs/quote-tab/quote-sub-section.tsx
@@ -1,13 +1,13 @@
+import styled from '@emotion/styled'
 import { Quote } from 'api/generated/graphql'
 import { CreateQuoteForm } from 'components/member/tabs/quote-tab/create-quote-form'
 import { Button } from 'hedvig-ui/button'
 import { Card, CardsWrapper } from 'hedvig-ui/card'
 import { MainHeadline } from 'hedvig-ui/typography'
 import React from 'react'
-import styled from '@emotion/styled'
 import { showNotification } from 'store/actions/notificationsActions'
 import { ContractType } from 'types/enums'
-import { isSigned, isSignedOrExpired } from 'utils/quote'
+import { isSignedOrExpired } from 'utils/quote'
 import { ActionsWrapper, Muted } from './common'
 import { QuoteListItem } from './quote-list-item'
 
@@ -20,16 +20,11 @@ export const QuotesSubSection: React.FC<{
 }> = ({ memberId, contractType, quotes }) => {
   const [isWip, setIsWip] = React.useState(false)
   const activeQuotes = quotes.filter((quote) => !isSignedOrExpired(quote))
-  const signedQuotes = quotes.filter(isSigned)
-  const hasActiveQuotes = activeQuotes.length > 0
-  const hasSignedQuotes = signedQuotes.length > 0
 
   return (
     <Wrapper>
-      {!hasActiveQuotes && !hasSignedQuotes && (
-        <Button onClick={() => setIsWip(!isWip)}>Create</Button>
-      )}
-      {!hasActiveQuotes && !hasSignedQuotes && isWip && (
+      {<Button onClick={() => setIsWip(!isWip)}>Create</Button>}
+      {isWip && (
         <ActionsWrapper>
           <CreateQuoteForm
             memberId={memberId}
