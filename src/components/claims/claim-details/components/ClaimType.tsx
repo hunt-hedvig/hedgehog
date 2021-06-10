4292bfbcc04a23e81d7f12cc9869c2d63a954438
rasmusguterstam <rasmus@hedvig.com>
2021-06-10 15:40:24 +0200

    Searchable claim types, fix full width for date-time-picker, add badge to title

--- src/components/claims/claim-details/components/ClaimType.tsx
+++ src/components/claims/claim-details/components/ClaimType.tsx
@@ -6,20 +6,35 @@ import {
   useSetClaimInformationMutation,
   useSetClaimTypeMutation,
 } from 'api/generated/graphql'
+import Select from 'react-select'
 
+import {
+  PaperTitle,
+  PaperTitleBadgeProps,
+} from 'components/claims/claim-details/components/claim-items/PaperTitle'
 import { FieldSelect } from 'components/shared/inputs/FieldSelect'
 import { TextField } from 'components/shared/inputs/TextField'
 import { Paper } from 'components/shared/Paper'
 import { format, parseISO } from 'date-fns'
 import { Field, Form, Formik } from 'formik'
-import { FormikDateTimePicker } from 'hedvig-ui/date-time-picker'
+import {
+  DateTimePicker,
+  FormikDateTimePicker,
+} from 'hedvig-ui/date-time-picker'
+import { Input } from 'hedvig-ui/input'
 import { Loadable } from 'hedvig-ui/loadable'
 import { Spinner } from 'hedvig-ui/sipnner'
-import { ErrorText, Paragraph, ThirdLevelHeadline } from 'hedvig-ui/typography'
-import React from 'react'
+import {
+  ErrorText,
+  Label,
+  Paragraph,
+  ThirdLevelHeadline,
+} from 'hedvig-ui/typography'
+import React, { useState } from 'react'
+import { BugFill, ExclamationCircleFill } from 'react-bootstrap-icons'
 import { WithShowNotification } from 'store/actions/notificationsActions'
 import { withShowNotification } from 'utils/notifications'
-import { getCarrierText } from 'utils/text'
+import { convertCamelcaseToTitle, getCarrierText } from 'utils/text'
 
 const hasLocation = (typename: ClaimTypes): boolean => {
   return [
@@ -73,15 +88,7 @@ interface ClaimTypeProps {
   memberId: string
 }
 
-const Label = styled.label`
-  display: block;
-`
-
 const SubmitButton = MuiButton
-
-const ClaimTypeInformationForm = styled(Form)({
-  marginTop: '1rem',
-})
 
 const handleError = (showNotification: (data: any) => void) => () => {
   showNotification({
@@ -95,11 +102,8 @@ const DangerParagraph = styled(Paragraph)`
   color: ${({ theme }) => theme.danger};
 `
 
-const ClaimTypeComponent: React.FC<ClaimTypeProps & WithShowNotification> = ({
-  claimId,
-  memberId,
-  showNotification,
-}) => {
+const ClaimTypeOLDComponent: React.FC<ClaimTypeProps &
+  WithShowNotification> = ({ claimId, memberId, showNotification }) => {
   const [isSetClaimTypeLoading, setSetClaimTypeLoading] = React.useState(false)
   const {
     data: claimInformationData,
@@ -192,12 +196,12 @@ const ClaimTypeComponent: React.FC<ClaimTypeProps & WithShowNotification> = ({
             ticket?: string
           }>
             initialValues={{
-              location: typeAny?.location || '',
-              date: typeAny?.date ? parseISO(typeAny.date) : null,
-              item: typeAny?.item || '',
-              policeReport: typeAny?.policeReport || '',
-              receipt: typeAny?.receipt || '',
-              ticket: typeAny?.ticket || '',
+              location: typea?.location || '',
+              date: type?.date ? parseISO(type.date) : null,
+              item: type?.item || '',
+              policeReport: type?.policeReport || '',
+              receipt: type?.receipt || '',
+              ticket: type?.ticket || '',
             }}
             onSubmit={async (values) => {
               await setClaimInformation({
@@ -303,6 +307,190 @@ const ClaimTypeComponent: React.FC<ClaimTypeProps & WithShowNotification> = ({
           </Formik>
         ) : null}
       </Loadable>
+    </Paper>
+  )
+}
+
+export const SearchableDropdown = styled(Select)`
+  .searchable-type-select__control {
+    margin-top: 1px;
+    border-radius: 7px;
+    height: 44px;
+    box-shadow: none;
+    background-color: ${({ theme }) => theme.backgroundLight};
+    border: 1px solid ${({ theme }) => theme.border};
+    font-size: 1rem;
+  }
+
+  .searchable-type-select__input {
+    color: ${({ theme }) => theme.foreground};
+    padding-left: 0px;
+  }
+
+  .searchable-type-select__menu {
+    border-radius: 0;
+    hyphens: auto;
+    margin-top: 0px;
+    text-align: left;
+    word-wrap: break-word;
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.foreground};
+  }
+
+  .searchable-type-select__option {
+    &:hover {
+      background: ${({ theme }) => theme.accentBackground};
+    }
+  }
+
+  .searchable-type-select__value-container {
+    padding-left: 16px;
+    overflow: visible;
+  }
+
+  .searchable-type-select__multi-value__remove {
+    display: none;
+  }
+
+  .searchable-type-select__single-value {
+    color: ${({ theme }) => theme.foreground};
+  }
+`
+const createClaimTypeOption = (claimType: string) => ({
+  value: claimType,
+  label: convertCamelcaseToTitle(claimType),
+  searchTerms: claimType,
+})
+
+const ClaimTypeDataForm: React.FC<{ type: any }> = ({ type }) => {
+  const [setClaimInformation] = useSetClaimInformationMutation()
+
+  const [formData, setFormData] = useState({
+    location: type?.location || '',
+    date: type?.date ? parseISO(type.date) : null,
+    item: type?.item || '',
+    policeReport: type?.policeReport || '',
+    receipt: type?.receipt || '',
+    ticket: type?.ticket || '',
+  })
+
+  return (
+    <>
+      {hasLocation(type.__typename as ClaimTypes) && (
+        <div style={{ marginTop: '1.0em' }}>
+          <Label>Location</Label>
+          <Input placeholder={'Any specific location?'} />
+        </div>
+      )}
+      <div style={{ marginTop: '1.0em' }}>
+        <Label>Date of Occurrence</Label>
+        <DateTimePicker
+          fullWidth={true}
+          date={formData.date ?? new Date()}
+          setDate={(newDate) => setFormData({ ...formData, date: newDate })}
+        />
+      </div>
+      {hasItem(type.__typename as ClaimTypes) && (
+        <div style={{ marginTop: '1.0em' }}>
+          <Label>Item</Label>
+          <Input placeholder={'Any specific item?'} />
+        </div>
+      )}
+      {hasPoliceReport(type.__typename as ClaimTypes) && (
+        <div style={{ marginTop: '1.0em' }}>
+          <Label>Police report</Label>
+          <Input placeholder={'Has a police report been filed?'} />
+        </div>
+      )}
+      {hasReceipt(type.__typename as ClaimTypes) && (
+        <div style={{ marginTop: '1.0em' }}>
+          <Label>Receipt</Label>
+          <Input placeholder={'Is there a receipt?'} />
+        </div>
+      )}
+      {hasTicket(type.__typename as ClaimTypes) && (
+        <div style={{ marginTop: '1.0em' }}>
+          <Label>Ticket</Label>
+          <Input placeholder={'Is there a ticket?'} />
+        </div>
+      )}
+    </>
+  )
+}
+
+const ClaimTypeComponent: React.FC<{
+  claimId: string
+  memberId: string
+} & WithShowNotification> = ({ claimId, memberId, showNotification }) => {
+  const {
+    data: claimInformationData,
+    loading: loadingClaimInformation,
+    refetch,
+    error: queryError,
+  } = useClaimInformationQuery({
+    variables: { claimId, memberId },
+  })
+
+  const [
+    setClaimType,
+    { loading: setClaimTypeLoading },
+  ] = useSetClaimTypeMutation()
+
+  const { contract, type } = claimInformationData?.claim ?? {}
+
+  const titleBadge = (): PaperTitleBadgeProps | null => {
+    if (queryError) {
+      return {
+        icon: BugFill,
+        status: 'danger',
+        label: 'Internal Error',
+      }
+    }
+    if (!contract && !loadingClaimInformation) {
+      return {
+        icon: ExclamationCircleFill,
+        status: 'warning',
+        label: 'No contract set',
+      }
+    }
+
+    return null
+  }
+
+  return (
+    <Paper>
+      <PaperTitle title={'Claim Type'} badge={titleBadge()} />
+      <SearchableDropdown
+        value={
+          type?.__typename &&
+          createClaimTypeOption(type?.__typename?.toString())
+        }
+        classNamePrefix="searchable-type-select"
+        placeholder={'What type of claim is this?'}
+        isLoading={setClaimTypeLoading || loadingClaimInformation}
+        isClearable={true}
+        isSearchable={true}
+        onChange={(selection) => {
+          setClaimType({
+            variables: { id: claimId, type: selection?.value ?? null },
+          })
+            .then(() => {
+              refetch()
+            })
+            .catch(() => {
+              showNotification({
+                header: 'Error',
+                type: 'red',
+                message: 'Something went wrong, notify the tech team',
+              })
+            })
+        }}
+        noOptionsMessage={() => 'No types found'}
+        options={Object.keys(ClaimTypes).map((claimType) =>
+          createClaimTypeOption(claimType),
+        )}
+      />
+      {!!type && <ClaimTypeDataForm type={type} />}
     </Paper>
   )
 }
---
+++ src/components/claims/claim-details/components/claim-items/PaperTitle.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled'
+import { InfoTag, InfoTagStatus } from 'hedvig-ui/info-row'
+import { ThirdLevelHeadline } from 'hedvig-ui/typography'
+import React from 'react'
+
+export interface PaperTitleBadgeProps {
+  icon?: React.ElementType
+  status: InfoTagStatus
+  label: string
+}
+
+const PaperTitleWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1em;
+`
+
+export const PaperTitle: React.FC<{
+  title: string
+  badge?: PaperTitleBadgeProps | null
+}> = ({ title, badge }) => {
+  const BadgeIcon = badge?.icon
+
+  return (
+    <PaperTitleWrapper>
+      <div>
+        <ThirdLevelHeadline>{title}</ThirdLevelHeadline>
+      </div>
+      {badge && (
+        <InfoTag
+          style={{
+            fontWeight: 'bold',
+            padding: '0.2em 0.7em',
+            fontSize: '0.85em',
+          }}
+          status={badge.status}
+        >
+          {BadgeIcon && (
+            <BadgeIcon
+              style={{
+                paddingTop: '0.2em',
+                marginRight: '0.5em',
+              }}
+            />
+          )}
+          {badge.label}
+        </InfoTag>
+      )}
+    </PaperTitleWrapper>
+  )
+}
