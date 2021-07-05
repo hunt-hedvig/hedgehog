b5d9514ccdcae884c0ffce3d50e23b705d2dda88
vonElfvin <elvin.granat@gmail.com>
2021-07-05 16:45:02 +0200

    changelog

--- src/changelog.ts
+++ src/changelog.ts
@@ -12,6 +12,11 @@ export interface Change {
 
 export const changelog: ReadonlyArray<Change> = [
   {
+    date: '2021-07-05',
+    change: 'Updated the Switcher Tool with more information and market filter',
+    authorGithubHandle: 'vonElfvin',
+  },
+  {
     date: '2021-07-01',
     change: 'New claim types "Other" and "Duplicate"',
     authorGithubHandle: 'vonElfvin',
