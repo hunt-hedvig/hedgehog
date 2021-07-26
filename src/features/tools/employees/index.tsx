781b3b40904aa9218867bc61f0df25b2588ff8f0
Anton <anton@hedvig.com>
2021-07-26 11:34:46 +0200

    create employee page

---
+++ src/features/tools/employees/index.tsx
@@ -0,0 +1,10 @@
+import { MainHeadline } from 'hedvig-ui/typography'
+import React from 'react'
+
+export const EmployeesComponent: React.FC = () => {
+  return (
+    <>
+      <MainHeadline>Employees</MainHeadline>
+    </>
+  )
+}
