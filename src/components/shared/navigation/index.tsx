95b00243f88565ad3dbd385f94180b26339e550e
rasmusguterstam <rasmus@hedvig.com>
2021-09-01 18:28:49 +0200

    Remove /store/*, restructure slightly

--- src/components/shared/navigation/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { VerticalMenu } from './sidebar/VerticalMenu'
-
-const menuIsHidden = (path) => path.startsWith('/login')
-
-export const Navigation = ({ history, store }) =>
-  !menuIsHidden(history.location.pathname) ? (
-    <VerticalMenu store={store} history={history} />
-  ) : null
