22d83d9b3d4fce9ad909ca0505edb09726fad033
Max <maxim.pletenev@fastdev.se>
2021-09-02 11:22:21 +0500

    update payout form with hedvig ui and useForm

--- src/components/payouts/payout-details/index.tsx
+++ src/components/payouts/payout-details/index.tsx
@@ -1,10 +1,4 @@
 import { ApolloCache } from '@apollo/client'
-import styled from '@emotion/styled'
-import {
-  Button as MuiButton,
-  MenuItem as MuiMenuItem,
-  withStyles,
-} from '@material-ui/core'
 import {
   GetMemberTransactionsDocument,
   GetMemberTransactionsQuery,
@@ -12,36 +6,31 @@ import {
   useGetContractMarketInfoQuery,
   usePayoutMemberMutation,
 } from 'api/generated/graphql'
-import { Field, Form, Formik } from 'formik'
+import { Form, FormDropdown, FormInput, SubmitButton } from 'hedvig-ui/form'
 import React, { useState } from 'react'
+import { FieldValues, FormProvider, useForm } from 'react-hook-form'
 import { toast } from 'react-hot-toast'
-import * as yup from 'yup'
-import { FieldSelect } from '../../shared/inputs/FieldSelect'
-import { TextField } from '../../shared/inputs/TextField'
 
-const SubmitButton = withStyles({
-  root: {
-    marginTop: '1rem',
-    display: 'block',
+const entryTypeOptions = [
+  {
+    key: 1,
+    value: 'MARKETING',
+    text: 'Marketing',
   },
-})(MuiButton)
-
-const VerticalForm = styled(Form)`
-  display: flex;
-  flex-direction: column;
-`
-
-const getPayoutValidationSchema = () =>
-  yup.object().shape({
-    amount: yup.number().required(),
-    category: yup
-      .string()
-      .oneOf(['MARKETING', 'REFERRAL', 'REFUND'])
-      .required(),
-    referenceId: yup.string().required(),
-  })
+  {
+    key: 2,
+    value: 'REFERRAL',
+    text: 'Referral',
+  },
+  {
+    key: 3,
+    value: 'REFUND',
+    text: 'Refund',
+  },
+]
 
 export const PayoutDetails: React.FC<{ memberId: string }> = ({ memberId }) => {
+  const form = useForm()
   const [confirmed, setConfirmed] = useState(false)
   const [payoutMember] = usePayoutMemberMutation()
   const { data: contractMarketInfo } = useGetContractMarketInfoQuery({
@@ -75,88 +64,95 @@ export const PayoutDetails: React.FC<{ memberId: string }> = ({ memberId }) => {
     })
   }
 
-  return (
-    <Formik
-      initialValues={{ category: '', amount: '', note: '', referenceId: '' }}
-      onSubmit={(data: any, { resetForm }) => {
-        if (!confirmed) {
-          return
-        }
+  const onSubmitHandler = (data: FieldValues) => {
+    if (!confirmed) {
+      setConfirmed((currentConfirmed) => !currentConfirmed)
+      return
+    }
 
-        toast.promise(
-          payoutMember({
-            variables: {
-              memberId,
-              request: {
-                amount: {
-                  amount: data.amount,
-                  currency: preferredCurrency,
-                },
-                category: data.category,
-                referenceId: data.referenceId,
-                note: data.note,
-              },
+    toast.promise(
+      payoutMember({
+        variables: {
+          memberId,
+          request: {
+            amount: {
+              amount: data.amount,
+              currency: preferredCurrency,
             },
-            update: (cache, { data: response }) => {
-              if (!response) {
-                return
-              }
-              updateCache(cache, response)
-            },
-          }),
-          {
-            loading: 'Creating payout...',
-            success: () => {
-              resetForm()
-              setConfirmed(false)
-              return 'Payout created'
-            },
-            error: 'Could not create payout',
+            category: data.category,
+            referenceId: data.referenceId,
+            note: data.note,
           },
-        )
-      }}
-      validationSchema={getPayoutValidationSchema()}
-    >
-      {({ isValid }) => (
-        <VerticalForm onChange={() => setConfirmed(false)}>
-          <Field component={FieldSelect} name="category">
-            <MuiMenuItem value="MARKETING">Marketing</MuiMenuItem>
-            <MuiMenuItem value="REFERRAL">Referral</MuiMenuItem>
-            <MuiMenuItem value="REFUND">Refund</MuiMenuItem>
-          </Field>
-          <Field component={TextField} label="Payout amount" name="amount" />
-          <Field
-            component={TextField}
-            label="Reference Id"
-            name="referenceId"
-          />
+        },
+        update: (cache, { data: response }) => {
+          if (!response) {
+            return
+          }
+          updateCache(cache, response)
+        },
+      }),
+      {
+        loading: 'Creating payout...',
+        success: () => {
+          form.reset()
+          setConfirmed(false)
+          return 'Payout created'
+        },
+        error: 'Could not create payout',
+      },
+    )
+  }
 
-          <Field component={TextField} label="Note" name="note" />
-          {!confirmed ? (
-            <SubmitButton
-              type="button"
-              variant="contained"
-              color="secondary"
-              disabled={!isValid}
-              onClick={(e) => {
-                e.preventDefault()
-                setConfirmed((currentConfirmed) => !currentConfirmed)
-              }}
-            >
-              Confirm payout
-            </SubmitButton>
-          ) : (
-            <SubmitButton
-              type="submit"
-              variant="contained"
-              color="primary"
-              disabled={!isValid}
-            >
-              Create payout
-            </SubmitButton>
-          )}
-        </VerticalForm>
-      )}
-    </Formik>
+  return (
+    <FormProvider {...form}>
+      <Form onSubmit={onSubmitHandler}>
+        <FormDropdown
+          label="Category"
+          options={entryTypeOptions}
+          name="category"
+          defaultValue=""
+          rules={{
+            required: 'Category is required',
+          }}
+        />
+        <FormInput
+          label="Payout amount"
+          name="amount"
+          defaultValue=""
+          type="number"
+          affix={{
+            content: preferredCurrency,
+            basic: true,
+          }}
+          affixPosition="right"
+          rules={{
+            required: 'Amount is required',
+            pattern: {
+              value: /[^0]/,
+              message: 'Amount cannot be zero',
+            },
+          }}
+        />
+        <FormInput
+          label="Reference Id"
+          name="referenceId"
+          defaultValue=""
+          rules={{
+            required: 'Reference Id is required',
+            maxLength: {
+              value: 50,
+              message: 'The reference is too long (max 50 characters)',
+            },
+          }}
+        />
+        <FormInput label="Note" name="note" defaultValue="" />
+
+        {!confirmed ? (
+          <SubmitButton variation="secondary">Confirm payout</SubmitButton>
+        ) : (
+          <SubmitButton variation="primary">Create payout</SubmitButton>
+        )}
+      </Form>
+    </FormProvider>
   )
 }
