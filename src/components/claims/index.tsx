233ee18357848e7e68b0ec8f5bdb67d953504356
rasmusguterstam <rasmus@hedvig.com>
2021-09-03 16:40:37 +0200

    Add draft of new table design

--- src/components/claims/index.tsx
+++ src/components/claims/index.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import { Claim } from 'api/generated/graphql'
 import { ListPage } from 'components/shared'
 import { Paginator } from 'components/shared/paginator/Paginator'
@@ -11,6 +12,80 @@ import { Header } from 'semantic-ui-react'
 import { useVerticalKeyboardNavigation } from 'utils/keyboard-actions'
 import { ClaimListHeader } from './claims-list/components/ClaimListHeader'
 import { ClaimListItem } from './claims-list/components/ClaimListItem'
+
+const Table = styled.table`
+  margin-top: 2em;
+  font-weight: normal;
+  text-align: left;
+  width: 100%;
+  border-collapse: collapse;
+
+  th {
+    font-weight: lighter;
+    color: ${({ theme }) => theme.semiStrongForeground};
+    font-size: 0.8em;
+    padding: 0.5em 1em 0.5em 1.2em;
+    background-color: ${({ theme }) => theme.accentSecondary};
+  }
+
+  th:first-child {
+    border-radius: 8px 0 0 8px;
+  }
+
+  th:last-child {
+    border-radius: 0 8px 8px 0;
+  }
+
+  tr {
+    width: 100%;
+    background-color: transparent;
+    transition: all 150ms;
+
+    :hover {
+      cursor: pointer;
+      background-color: ${({ theme }) => theme.accentSecondary};
+    }
+  }
+
+  td {
+    padding: 1.6em 1em;
+    font-size: 1.05em;
+    transition: all 100ms;
+    border-bottom: 1px solid ${({ theme }) => theme.accentSecondary};
+  }
+
+  td:first-child {
+    :hover {
+      border-radius: 8px 0 0 8px;
+    }
+  }
+
+  td:last-child {
+    :hover {
+      border-radius: 0 8px 8px 0;
+    }
+  }
+`
+
+const Badge = styled.span`
+  padding: 0.3em 1em;
+  background-color: ${({ theme }) => theme.lightSuccess};
+  color: ${({ theme }) => theme.success};
+  font-weight: bold;
+  font-size: 0.7em;
+  border-radius: 8px;
+`
+
+const GroupTag = styled.span`
+  font-size: 0.6em;
+  background-color: ${({ theme }) => theme.lightSuccess};
+  font-weight: bold;
+  color: ${({ theme }) => theme.success};
+  padding: 0.2em 0.6em;
+  border-radius: 8px;
+  margin-left: 0.7em;
+  margin-right: -0.7em;
+`
 
 export const ClaimsList: React.FC<RouteComponentProps<{
   page?: string
@@ -40,6 +115,102 @@ export const ClaimsList: React.FC<RouteComponentProps<{
   useEffect(() => {
     listClaims({ page: selectedPage - 1 ?? 0 })
   }, [selectedPage])
+
+  return (
+    <ListPage>
+      <FadeIn>
+        <Header size="huge">Claims</Header>
+      </FadeIn>
+      {loading && !claims ? <LoadingMessage paddingTop={'25vh'} /> : <></>}
+
+      <Table>
+        <tr>
+          <th>Member</th>
+          <th>Date Registered</th>
+          <th>Claim Type</th>
+          <th>Claim State</th>
+          <th>Claim Reserves</th>
+        </tr>
+        <tr>
+          <td>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <span>Rasmus Guterstam</span>
+              <div>
+                <span style={{ fontSize: '0.8em', color: '#848484' }}>
+                  123456789
+                </span>
+                <GroupTag>First Group</GroupTag>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span>August 31, 2021</span>
+              <span style={{ fontSize: '0.8em', color: '#848484' }}>11:42</span>
+            </div>
+          </td>
+
+          <td>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span>Theft Claim</span>
+            </div>
+          </td>
+
+          <td>
+            <Badge>OPEN</Badge>
+          </td>
+
+          <td>
+            <span
+              style={{
+                borderBottom: '1px solid #222222',
+              }}
+            >
+              450 <span style={{ fontSize: '0.6em' }}>SEK</span>
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span>Rasmus Guterstam</span>
+              <div>
+                <span style={{ fontSize: '0.8em', color: '#848484' }}>
+                  123456789
+                </span>
+                <GroupTag>Second Group</GroupTag>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span>August 31, 2021</span>
+              <span style={{ fontSize: '0.8em', color: '#848484' }}>11:42</span>
+            </div>
+          </td>
+
+          <td>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <span>Theft Claim</span>
+            </div>
+          </td>
+
+          <td>
+            <Badge>OPEN</Badge>
+          </td>
+
+          <td>
+            <span style={{ color: '#aaaaaa' }}>Not specified</span>
+          </td>
+        </tr>
+      </Table>
+    </ListPage>
+  )
 
   return (
     <ListPage>
