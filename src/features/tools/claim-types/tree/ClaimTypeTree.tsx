92e7a1b2135069f820255667d3eca5db4bad16dc
rasmusguterstam <rasmus@hedvig.com>
2021-09-23 18:04:57 +0200

    Add working drag

--- src/features/tools/claim-types/tree/ClaimTypeTree.tsx
+++ src/features/tools/claim-types/tree/ClaimTypeTree.tsx
@@ -1,5 +1,54 @@
-import React from 'react'
+import { useDrag } from '@visx/drag'
+import { DefaultLink, DefaultNode, Graph } from '@visx/network'
+import React, { useState } from 'react'
 
 export const ClaimTypeTree: React.FC<{}> = ({}) => {
-  return null
+  const [nodes] = useState([
+    { x: 0, y: 0 },
+    { x: 50, y: 20 },
+    { x: 200, y: 300 },
+    { x: 300, y: 40 },
+  ])
+
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  const onDragMove = (currDrag) => {
+    setPosition(() => ({
+      x: currDrag.x - currDrag.dx,
+      y: currDrag.y - currDrag.dy,
+    }))
+  }
+
+  const { isDragging, dragStart, dragEnd, dragMove } = useDrag({
+    onDragMove,
+    resetOnStart: true,
+  })
+
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox={`${position.x} ${position.y} 1000 1000`}
+      style={{ borderRadius: '8px' }}
+      onMouseDown={dragStart}
+      onMouseUp={isDragging ? dragEnd : undefined}
+      onMouseMove={isDragging ? dragMove : undefined}
+      onTouchStart={dragStart}
+      onTouchEnd={isDragging ? dragEnd : undefined}
+      onTouchMove={isDragging ? dragMove : undefined}
+    >
+      <Graph
+        graph={{
+          nodes,
+          links: [
+            { source: nodes[0], target: nodes[1] },
+            { source: nodes[1], target: nodes[2] },
+            { source: nodes[2], target: nodes[0] },
+          ],
+        }}
+        linkComponent={DefaultLink}
+        nodeComponent={DefaultNode}
+      />
+    </svg>
+  )
 }
--- src/pages/tools/ClaimTypesPage.tsx
+++ src/pages/tools/ClaimTypesPage.tsx
@@ -1,15 +1,14 @@
 import styled from '@emotion/styled'
 import { Flex, MainHeadline, SecondLevelHeadline, Spacing } from '@hedvig-ui'
 import { CreateRelationForm } from 'features/tools/claim-types/CreateRelationForm'
+import { ClaimTypeTree } from 'features/tools/claim-types/tree/ClaimTypeTree'
 import React from 'react'
-import { ClaimTypeTree } from 'src/features/tools/claim-types/tree/ClaimTypeTree'
 
 const Overview = styled(Flex)`
   background-color: ${({ theme }) => theme.backgroundTransparent};
   width: 100%;
   height: 70vh;
   border-radius: 8px;
-  padding: 1em 1.3em;
 `
 
 export const ClaimTypesPage: React.FC = () => {
