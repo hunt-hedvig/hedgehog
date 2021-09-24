36c0eb2f6dd2411d9e16444573dc9928802632cd
rasmusguterstam <rasmus@hedvig.com>
2021-09-24 11:15:14 +0200

    Connect "Create relation" form with queries

--- src/features/tools/claim-types/CreateRelationForm.tsx
+++ src/features/tools/claim-types/CreateRelationForm.tsx
@@ -2,43 +2,167 @@ import {
   Button,
   Dropdown,
   Flex,
+  getTextFromEnumValue,
   Label,
   SearchableDropdown,
   Spacing,
 } from '@hedvig-ui'
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  useCreateClaimPropertyMutation,
+  useCreateClaimPropertyOptionMutation,
+  useGetClaimPropertiesQuery,
+  useGetClaimPropertyOptionQuery,
+  useGetClaimPropertyOptionsQuery,
+  useGetClaimTypesQuery,
+} from 'types/generated/graphql'
+
+const ClaimTypeDropdown: React.FC<{
+  value: string
+  onChange: (value: string) => void
+}> = ({ value, onChange }) => {
+  const { data } = useGetClaimTypesQuery()
+  const claimTypes = data?.claimTypes
+
+  if (!claimTypes) {
+    return null
+  }
+
+  return (
+    <Dropdown
+      value={value}
+      onChange={onChange}
+      options={claimTypes.map((type) => ({
+        value: type,
+        text: getTextFromEnumValue(type),
+      }))}
+    />
+  )
+}
+
+const ClaimPropertyDropdown: React.FC<{
+  value: string
+  onChange: (value: string) => void
+}> = ({ value, onChange }) => {
+  const [createClaimProperty, { loading }] = useCreateClaimPropertyMutation()
+  const { data } = useGetClaimPropertiesQuery()
+  const claimProperties = data?.claimProperties
+
+  if (!claimProperties) {
+    return null
+  }
+
+  return (
+    <div style={{ width: '100%' }}>
+      <SearchableDropdown
+        creatable={true}
+        formatCreateLabel={(optionValue) => (
+          <span>
+            Create property "<b>{optionValue}</b>"?
+          </span>
+        )}
+        onCreateOption={(option) => {
+          if (!option) {
+            return
+          }
+        }}
+        value={
+          value
+            ? {
+                value,
+                label: value,
+              }
+            : null
+        }
+        placeholder="Select property"
+        isLoading={loading}
+        isCreatable={true}
+        onChange={onChange}
+        noOptionsMessage={() => 'No properties found'}
+        options={claimProperties.map((property) => ({
+          value: property.id,
+          label: property.name,
+        }))}
+      />
+    </div>
+  )
+}
+
+const ClaimPropertyOptionDropdown: React.FC<{
+  value: string
+  onChange: (value: string) => void
+}> = ({ value, onChange }) => {
+  const [
+    createClaimPropertyOption,
+    { loading },
+  ] = useCreateClaimPropertyOptionMutation()
+  const { data } = useGetClaimPropertyOptionsQuery()
+  const claimPropertyOptions = data?.claimPropertyOptions
+
+  if (!claimPropertyOptions) {
+    return null
+  }
+
+  return (
+    <div style={{ width: '100%' }}>
+      <SearchableDropdown
+        creatable={true}
+        formatCreateLabel={(optionValue) => (
+          <span>
+            Create option "<b>{optionValue}</b>"?
+          </span>
+        )}
+        onCreateOption={(option) => {
+          if (!option) {
+            return
+          }
+        }}
+        value={
+          value
+            ? {
+                value,
+                label: value,
+              }
+            : null
+        }
+        placeholder="Select option"
+        isLoading={loading}
+        isCreatable={true}
+        onChange={onChange}
+        noOptionsMessage={() => 'No options found'}
+        options={claimPropertyOptions.map((option) => ({
+          value: option.id,
+          label: option.name,
+        }))}
+      />
+    </div>
+  )
+}
 
 export const CreateRelationForm: React.FC<{}> = () => {
+  const [claimType, setClaimType] = useState('')
+  const [claimPropertyId, setClaimPropertyId] = useState('')
+  const [claimPropertyOptionId, setClaimPropertyOptionId] = useState('')
+
   return (
     <Flex direction="column" fullWidth>
       <Label>Claim Type</Label>
-      <Dropdown value={''} onChange={() => void 0} options={[]} />
+      <ClaimTypeDropdown
+        value={claimType}
+        onChange={(value) => setClaimType(value)}
+      />
       <Spacing top={'small'} />
       <Label>Property</Label>
-      <div style={{ width: '100%' }}>
-        <SearchableDropdown
-          creatable={true}
-          value={{ label: 'Lol', value: 'Lol' }}
-          placeholder="Which incentive type?"
-          isClearable={true}
-          onChange={() => void 0}
-          noOptionsMessage={() => 'No incentive type found'}
-          options={[]}
-        />
-      </div>
+      <ClaimPropertyDropdown
+        value={claimPropertyId}
+        onChange={(value) => setClaimPropertyId(value)}
+      />
       <Spacing top={'small'} />
       <Label>Option</Label>
-      <div style={{ width: '100%' }}>
-        <SearchableDropdown
-          creatable={true}
-          value={{ label: 'Lol', value: 'Lol' }}
-          placeholder="Which incentive type?"
-          isClearable={true}
-          onChange={() => void 0}
-          noOptionsMessage={() => 'No incentive type found'}
-          options={[]}
-        />
-      </div>
+      <ClaimPropertyOptionDropdown
+        value={claimPropertyOptionId}
+        onChange={(value) => setClaimPropertyOptionId(value)}
+      />
       <Spacing top={'small'} />
       <Button variation="primary">Create</Button>
     </Flex>
