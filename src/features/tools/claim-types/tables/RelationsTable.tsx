1a2a085e665b27e22529b481555bcf38a4b8cbee
rasmusguterstam <rasmus@hedvig.com>
2021-09-27 16:44:34 +0200

    Implement "delete relation" mutation

--- src/features/tools/claim-types/tables/RelationsTable.tsx
+++ src/features/tools/claim-types/tables/RelationsTable.tsx
@@ -1,4 +1,6 @@
+import styled from '@emotion/styled'
 import {
+  Button,
   getTextFromEnumValue,
   Table,
   TableColumn,
@@ -7,9 +9,20 @@ import {
   TableRow,
 } from '@hedvig-ui'
 import React from 'react'
-import { useGetClaimTypeRelationsQuery } from 'types/generated/graphql'
+import { toast } from 'react-hot-toast'
+import {
+  GetClaimTypeRelationsDocument,
+  GetClaimTypeRelationsQuery,
+  useDeleteClaimTypeRelationMutation,
+  useGetClaimTypeRelationsQuery,
+} from 'types/generated/graphql'
+
+const NonClickableColumn = styled(TableColumn)`
+  cursor: default;
+`
 
 export const RelationsTable: React.FC<{ filter: string }> = ({ filter }) => {
+  const [deleteRelation, { loading }] = useDeleteClaimTypeRelationMutation()
   const { data } = useGetClaimTypeRelationsQuery()
   const claimTypeRelations = data?.claimTypeRelations
 
@@ -23,6 +36,7 @@ export const RelationsTable: React.FC<{ filter: string }> = ({ filter }) => {
         <TableHeaderColumn>Type</TableHeaderColumn>
         <TableHeaderColumn>Property</TableHeaderColumn>
         <TableHeaderColumn>Option</TableHeaderColumn>
+        <TableHeaderColumn />
       </TableHeader>
 
       {claimTypeRelations
@@ -52,15 +66,56 @@ export const RelationsTable: React.FC<{ filter: string }> = ({ filter }) => {
           return false
         })
         .map((relation) => (
-          <>
-            <TableRow key={relation.id}>
-              <TableColumn>
-                {getTextFromEnumValue(relation.claimType)}
-              </TableColumn>
-              <TableColumn>{relation.property.name}</TableColumn>
-              <TableColumn>{relation.propertyOption.name}</TableColumn>
-            </TableRow>
-          </>
+          <TableRow key={relation.id}>
+            <NonClickableColumn>
+              {getTextFromEnumValue(relation.claimType)}
+            </NonClickableColumn>
+            <NonClickableColumn>{relation.property.name}</NonClickableColumn>
+            <NonClickableColumn>
+              {relation.propertyOption.name}
+            </NonClickableColumn>
+            <NonClickableColumn style={{ textAlign: 'right' }}>
+              <Button
+                variation="danger"
+                disabled={loading}
+                onClick={() =>
+                  toast.promise(
+                    deleteRelation({
+                      variables: { id: relation.id },
+                      update: (cache, { data: response }) => {
+                        if (!response) {
+                          return
+                        }
+                        const cachedData = cache.readQuery({
+                          query: GetClaimTypeRelationsDocument,
+                        })
+
+                        const cachedRelations = (cachedData as GetClaimTypeRelationsQuery)
+                          .claimTypeRelations
+
+                        cache.writeQuery({
+                          query: GetClaimTypeRelationsDocument,
+                          data: {
+                            claimTypeRelations: cachedRelations.filter(
+                              (claimTypeRelation) =>
+                                claimTypeRelation.id !== relation.id,
+                            ),
+                          },
+                        })
+                      },
+                    }),
+                    {
+                      loading: 'Deleting relation',
+                      success: 'Relation deleted',
+                      error: 'Could not delete relation',
+                    },
+                  )
+                }
+              >
+                Delete
+              </Button>
+            </NonClickableColumn>
+          </TableRow>
         ))}
     </Table>
   )
