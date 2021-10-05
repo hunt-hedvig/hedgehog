d518cd3b316718f9f347d773e7c074b1c9c15f6d
Max <maxim.pletenev@fastdev.se>
2021-10-05 12:56:25 +0500

    Create first version of Dropdown component

--- shared/@hedvig-ui/Dropdown/dropdown.stories.tsx
+++ shared/@hedvig-ui/Dropdown/dropdown.stories.tsx
@@ -1,5 +1,15 @@
-import { EnumDropdown } from '@hedvig-ui'
+import {
+  Button,
+  EnumDropdown,
+  Spinner,
+  Table,
+  TableColumn,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+} from '@hedvig-ui'
 import React from 'react'
+import { Dropdown as DropdownNoSemantic, Option } from './dropdown2'
 
 export default {
   title: 'EnumDropdown',
@@ -27,5 +37,57 @@ export const DropdownWithEnum: React.FC = () => {
         {taste}
       </h3>
     </>
+  )
+}
+
+export const DropdownWithoutSemantic = () => {
+  const [selected, setSelected] = React.useState<number>()
+
+  const OPTIONS = [
+    <div style={{ display: 'flex', gap: 15 }}>
+      <Button tabIndex={-1}>Option #1</Button>
+      <Button tabIndex={-1}>Option #1</Button>
+      <Button tabIndex={-1}>Option #1</Button>
+      <Button tabIndex={-1}>Option #1</Button>
+    </div>,
+    <Table>
+      <TableHeader>
+        <TableHeaderColumn>Option #2</TableHeaderColumn>
+      </TableHeader>
+
+      <TableRow>
+        <TableColumn>{21}</TableColumn>
+        <TableColumn>{23}</TableColumn>
+        <TableColumn>{24}</TableColumn>
+        <TableColumn>{25}</TableColumn>
+      </TableRow>
+    </Table>,
+    <div style={{ display: 'flex', gap: 15 }}>
+      <Spinner />
+      <Spinner />
+      <Spinner />
+      <Spinner />
+      <Spinner />
+      <Spinner />
+    </div>,
+    'Option №4',
+    'Option №5',
+    'Option №6',
+    'Option №7',
+  ]
+
+  return (
+    <div style={{ padding: 50 }}>
+      <DropdownNoSemantic title="Dropdown">
+        {OPTIONS.map((opt, index) => (
+          <Option
+            selected={selected === index}
+            onClick={() => setSelected(index)}
+          >
+            {opt}
+          </Option>
+        ))}
+      </DropdownNoSemantic>
+    </div>
   )
 }
---
+++ shared/@hedvig-ui/Dropdown/dropdown2.tsx
@@ -0,0 +1,193 @@
+import { keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import React, { LiHTMLAttributes, useEffect } from 'react'
+import { Keys } from '../utils/key-press-hook'
+import { sleep } from '../utils/sleep'
+
+const show = keyframes`
+  from {
+    opacity: 0;
+    transform: translate(0, -50px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
+`
+
+const close = keyframes`
+  from {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
+
+  to {
+    opacity: 0;
+    transform: translate(0, -50px);
+  }
+`
+
+const DropdownStyled = styled.div<{ active: boolean }>`
+  position: relative;
+  & ul,
+  & li {
+    list-style: none;
+  }
+
+  & > li:first-child {
+    ${({ active }) =>
+      active &&
+      `
+    border-bottom: none;
+    border-radius: 0.3rem 0.3rem 0 0;
+  `}
+  }
+`
+
+const OptionsList = styled.ul<{ closing: boolean }>`
+  margin: 0;
+  position: absolute;
+  width: 100%;
+
+  animation: ${({ closing }) => (!closing ? show : close)} 0.1s linear;
+  padding: 15px;
+  background-color: ${({ theme }) => theme.backgroundLight};
+  border-radius: 0 0 0.3rem 0.3rem;
+  border: 1px solid ${({ theme }) => theme.border};
+  border-top: none;
+
+  & li:not(:first-child) {
+    margin-top: 1rem;
+  }
+`
+
+const OptionStyled = styled.li`
+  outline: none;
+  cursor: pointer;
+  padding: 10px 25px;
+  border-radius: 0.3rem;
+  border: 1px solid ${({ theme }) => theme.border};
+  background-color: ${({ theme }) => theme.backgroundLight};
+
+  &:hover,
+  &:focus {
+    background-color: ${({ theme }) => theme.accentBackground};
+  }
+`
+
+interface DropdownProps {
+  title?: string
+  children: any
+}
+
+export const Dropdown: React.FC<DropdownProps> = ({ title, children }) => {
+  const [selectedIdx, setSelectedIdx] = React.useState()
+  const [active, setActive] = React.useState(false)
+  const [closing, setClosing] = React.useState(false)
+
+  const toggleDropdown = async () => {
+    if (active) {
+      setClosing(true)
+      await sleep(90)
+      setClosing(false)
+      setActive(false)
+      return
+    }
+
+    setActive(true)
+  }
+
+  useEffect(() => {
+    children.forEach((el, index) => {
+      if (el.props.selected) {
+        setSelectedIdx(index + 1)
+      }
+    })
+
+    console.log(children)
+  }, [children])
+
+  return (
+    <DropdownStyled
+      active={active}
+      onKeyDown={(e) => {
+        if (e.keyCode === Keys.Escape.code) {
+          toggleDropdown()
+          return
+        }
+      }}
+    >
+      {!selectedIdx ? (
+        <OptionStyled
+          tabIndex={0}
+          onClick={toggleDropdown}
+          onKeyDown={(e) => {
+            if (e.keyCode === Keys.Enter.code) {
+              toggleDropdown()
+              return
+            }
+          }}
+        >
+          {title || 'Dropdown'}
+        </OptionStyled>
+      ) : (
+        {
+          ...children[selectedIdx - 1],
+          props: {
+            ...children[selectedIdx - 1].props,
+            onClick: () => {
+              children[selectedIdx - 1].props.onClick()
+              toggleDropdown()
+            },
+          },
+        }
+      )}
+
+      {active && (
+        <OptionsList closing={closing}>
+          {children.map((el) => {
+            const newEl = {
+              ...el,
+              props: {
+                ...el.props,
+                onClick: () => {
+                  el.props.onClick()
+                  toggleDropdown()
+                },
+              },
+            }
+
+            return newEl
+          })}
+        </OptionsList>
+      )}
+    </DropdownStyled>
+  )
+}
+
+export interface OptionProps
+  extends Omit<LiHTMLAttributes<HTMLLIElement>, 'onClick'> {
+  onClick: () => void
+  selected: boolean
+}
+
+export const Option: React.FC<OptionProps> = ({
+  children,
+  onClick,
+  ...props
+}) => (
+  <OptionStyled
+    tabIndex={0}
+    onKeyDown={(e) => {
+      if (e.keyCode === Keys.Enter.code) {
+        onClick()
+        return
+      }
+    }}
+    onClick={onClick}
+    {...props}
+  >
+    {children}
+  </OptionStyled>
+)
