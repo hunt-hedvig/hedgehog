a53ceeeae26be098c9a307a961b350636c7ff8d3
Max <maxim.pletenev@fastdev.se>
2021-10-15 11:08:00 +0500

    Text datepicker improvement

--- shared/@hedvig-ui/TextDatePicker/index.tsx
+++ shared/@hedvig-ui/TextDatePicker/index.tsx
@@ -1,12 +1,34 @@
 import styled from '@emotion/styled'
-import { Input, Shadowed } from '@hedvig-ui'
+import { Input } from '@hedvig-ui'
 import nlp from 'compromise'
 import dates from 'compromise-dates'
 import numbers from 'compromise-numbers'
+import { parseISO } from 'date-fns'
 import formatDate from 'date-fns/format'
 import React, { HTMLAttributes } from 'react'
+import { CalendarDate } from 'react-bootstrap-icons'
+import DatePicker from 'react-datepicker'
 import { Keys } from '../utils/key-press-hook'
-import { usePlatform } from '../utils/platform'
+
+const Wrapper = styled.div`
+  width: fit-content;
+  position: relative;
+
+  & .react-datepicker {
+    position: absolute;
+    top: 60px;
+  }
+`
+
+const ErrorMessage = styled.span`
+  position: absolute;
+
+  font-size: 14px;
+  color: ${({ theme }) => theme.danger};
+
+  bottom: -25px;
+  right: 10px;
+`
 
 export const getDate = (value: string) => {
   nlp.extend(numbers)
@@ -20,59 +42,79 @@ export const getDate = (value: string) => {
   return date
 }
 
-const ChatTip = styled.div`
-  margin-top: 10px;
-  font-size: 0.8em;
-  color: ${({ theme }) => theme.semiStrongForeground};
-`
-
 interface TextDatePickerProps
   extends Omit<HTMLAttributes<HTMLInputElement>, 'value'> {
-  value: string
-  setValue: any
-  setDate: any
+  value?: Date | null
+  setValue: (date: Date | null) => void
 }
 
 export const TextDatePicker: React.FC<TextDatePickerProps> = ({
   value,
   setValue,
-  setDate,
   ...props
 }) => {
-  const [textFieldFocused, setTextFieldFocused] = React.useState(false)
-  const { isMetaKey, metaKey } = usePlatform()
+  const [isOldDatepicker, setIsOldDatepicker] = React.useState(false)
+  const [error, setError] = React.useState(false)
+  const [textValue, setTextValue] = React.useState(
+    value?.toISOString()?.split('T')[0] ?? null,
+  )
 
   const setDateHandler = () => {
-    const date = getDate(value)
+    const date = getDate(textValue || '')
 
     if (date) {
-      const newDate = new Date(date.start)
-      const formattedDate = formatDate(newDate, 'yyyy-MM-dd')
+      setError(false)
+      const newDate = date.start
 
-      setDate(formattedDate)
+      const isoDate = parseISO(newDate)
+      const formattedDate = formatDate(isoDate, 'yyyy-MM-dd')
+
+      setValue(new Date(newDate))
+      setTextValue(formattedDate)
+    } else {
+      setValue(null)
+      setError(true)
     }
   }
 
   return (
-    <div>
+    <Wrapper>
       <Input
-        onFocus={() => setTextFieldFocused(true)}
-        onBlur={() => setTextFieldFocused(false)}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
+        error={error}
+        icon={
+          <CalendarDate
+            onClick={() => setIsOldDatepicker((prev) => !prev)}
+            style={{ cursor: 'pointer' }}
+          />
+        }
+        size="large"
+        onBlur={() => {
+          setDateHandler()
+        }}
+        value={textValue || ''}
+        onChange={(e) => setTextValue(e.target.value)}
         onKeyDown={(e) => {
-          if (isMetaKey(e) && e.keyCode === Keys.Enter.code && value) {
+          if (e.keyCode === Keys.Enter.code && textValue) {
             setDateHandler()
           }
         }}
         {...props}
       />
-      {textFieldFocused && (
-        <ChatTip>
-          Press <Shadowed>{metaKey.hint}</Shadowed> + <Shadowed>Enter</Shadowed>{' '}
-          to get date
-        </ChatTip>
+      {error && <ErrorMessage>Invalid date</ErrorMessage>}
+      {isOldDatepicker && (
+        <DatePicker
+          inline
+          date={value || null}
+          onChange={(date) => {
+            setError(false)
+            const newDate = parseISO(date.toISOString())
+            const formattedDate = formatDate(newDate, 'yyyy-MM-dd')
+            setValue(date)
+            setTextValue(formattedDate)
+            setIsOldDatepicker(false)
+          }}
+        />
       )}
-    </div>
+    </Wrapper>
   )
 }
--- shared/@hedvig-ui/TextDatePicker/text-datepicker.stories.tsx
+++ shared/@hedvig-ui/TextDatePicker/text-datepicker.stories.tsx
@@ -7,22 +7,19 @@ export default {
 }
 
 export const StandardTextDatePicker: React.FC = () => {
-  const [value, setValue] = React.useState<string>('')
-  const [date, setDate] = React.useState<string>('')
+  const [value, setValue] = React.useState<Date | null>()
 
   return (
-    <>
+    <div style={{ padding: 30 }}>
       <TextDatePicker
         value={value}
-        setValue={setValue}
-        setDate={setDate}
+        setValue={(date: Date | null) => setValue(date)}
         placeholder="Enter date string"
+        style={{ width: 300 }}
       />
       <div style={{ marginTop: 20 }}>
-        <span>Text:</span> {value}
-        <br />
-        <span>Date:</span> {date}
+        <span>Date:</span> {value?.toDateString()}
       </div>
-    </>
+    </div>
   )
 }
