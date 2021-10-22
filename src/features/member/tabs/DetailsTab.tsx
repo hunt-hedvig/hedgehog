ef200d4d7430098492706dbd25fc840eaf89821b
Max <maxim.pletenev@fastdev.se>
2021-10-22 09:26:16 +0500

    Set uppercase of name in Member edit

--- src/features/member/tabs/DetailsTab.tsx
+++ src/features/member/tabs/DetailsTab.tsx
@@ -54,10 +54,20 @@ export const DetailsTab: React.FC<{
     }
   }
 
-  const handleChange = (field) => (e) => {
+  const setFieldValue = (field, value) => {
     const editedMemberDetails = { ...editMemberInfoRequest }
-    editedMemberDetails[field] = e.target.value
+    editedMemberDetails[field] = value
     setEditMemberInfoRequest(editedMemberDetails)
+  }
+
+  const handleChange = (field) => (e) => {
+    if (field === 'firstName' || field === 'lastName') {
+      const value =
+        e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
+      setFieldValue(field, value)
+    } else {
+      setFieldValue(field, e.target.value)
+    }
   }
 
   const handleCancel = () => {
