b976148e0721386cae8e0936dd79b6f353d98c76
Max <maxim.pletenev@fastdev.se>
2021-10-26 10:38:31 +0500

    Add loader to Conversations

--- src/features/member/messages/MessagesList.tsx
+++ src/features/member/messages/MessagesList.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Spinner } from '@hedvig-ui'
 import { parseISO } from 'date-fns'
 import { useMessageHistory } from 'graphql/use-message-history'
 import React, { HTMLAttributes } from 'react'
@@ -10,6 +11,13 @@ const MessagesListContainer = styled.div`
   overflow: auto;
   display: flex;
   flex-direction: column-reverse;
+`
+
+const MessagesListPlaceholder = styled.div`
+  height: 35em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const EmptyList = styled.h3`
@@ -26,23 +34,33 @@ export const MessagesList: React.FC<{
   const [messages, { loading }] = useMessageHistory(memberId)
 
   if (loading && !messages) {
-    return null
+    return (
+      <MessagesListPlaceholder>
+        <Spinner />
+      </MessagesListPlaceholder>
+    )
   }
 
   return (
     <>
       {messages ? (
-        <MessagesListContainer {...props}>
-          {messages.map((item, index) => (
-            <Message
-              key={`${item.globalId}-${index}`}
-              content={item.body}
-              left={item.fromId !== memberId}
-              timestamp={item.timestamp ? parseISO(item.timestamp) : null}
-              from={item.fromId === memberId ? null : getAuthor(item.author)}
-            />
-          ))}
-        </MessagesListContainer>
+        loading ? (
+          <MessagesListPlaceholder>
+            <Spinner />
+          </MessagesListPlaceholder>
+        ) : (
+          <MessagesListContainer {...props}>
+            {messages.map((item, index) => (
+              <Message
+                key={`${item.globalId}-${index}`}
+                content={item.body}
+                left={item.fromId !== memberId}
+                timestamp={item.timestamp ? parseISO(item.timestamp) : null}
+                from={item.fromId === memberId ? null : getAuthor(item.author)}
+              />
+            ))}
+          </MessagesListContainer>
+        )
       ) : (
         <EmptyList>No messages</EmptyList>
       )}
