1b22a59a29321e261a0eac77c29528eed9052e37
rasmusguterstam <rasmus@hedvig.com>
2021-10-27 14:37:12 +0200

    Try adding tsconfig.tsbuildinfo

--- webpack/webpack.config.base.js
+++ webpack/webpack.config.base.js
@@ -8,68 +8,61 @@ const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
 
 const smp = new SpeedMeasurePlugin()
 
-module.exports = ({
-  mode,
-  entry,
-  target,
-  plugins,
-  output,
-  context,
-  ...rest
-}) => smp.wrap({
-  mode,
-  resolve: {
-    extensions: ['.ts', '.tsx', '.mjs', '.js', '.json', '.css'],
-    modules: [
-      path.resolve(context, 'node_modules'),
-      path.resolve(context, 'src'),
-      path.resolve(context, 'shared'),
-    ],
-    symlinks: false,
-  },
-  entry,
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(tsx?|js)$/,
-        include: /(src|shared)/,
-        use: [
-          'thread-loader',
-          {
-            loader: 'babel-loader',
-            options: { ...babelrc, cacheDirectory: true },
-          },
-        ],
-      },
-      {
-        test: /\.m?js/,
-        resolve: {
-          fullySpecified: false,
-          unsafeCache: true,
+module.exports = ({ mode, entry, target, plugins, output, context, ...rest }) =>
+  smp.wrap({
+    mode,
+    resolve: {
+      extensions: ['.ts', '.tsx', '.mjs', '.js', '.json', '.css'],
+      modules: [
+        path.resolve(context, 'node_modules'),
+        path.resolve(context, 'src'),
+        path.resolve(context, 'shared'),
+      ],
+      symlinks: false,
+    },
+    entry,
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
         },
-      },
+        {
+          test: /\.(tsx?|js)$/,
+          include: /(src|shared)/,
+          use: [
+            'thread-loader',
+            {
+              loader: 'babel-loader',
+              options: { ...babelrc, cacheDirectory: true },
+            },
+          ],
+        },
+        {
+          test: /\.m?js/,
+          resolve: {
+            fullySpecified: false,
+            unsafeCache: true,
+          },
+        },
+      ],
+    },
+    target,
+    context,
+    stats: 'errors-only',
+    output,
+    plugins: [
+      /*
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: path.join(__dirname, '../build', 'vendor-manifest.json'),
+      }),
+      */
+      ...(plugins || []),
     ],
-  },
-  target,
-  context,
-  stats: 'errors-only',
-  output,
-  plugins: [
-    /*
-new webpack.DllReferencePlugin({
-  context: __dirname,
-  manifest: path.join(__dirname, '../build', 'vendor-manifest.json'),
-}),
-*/
-    ...(plugins || []),
-  ],
-  optimization: {
-    moduleIds: 'named',
-  },
-  bail: true,
-  ...rest,
-})
+    optimization: {
+      moduleIds: 'named',
+    },
+    bail: true,
+    ...rest,
+  })
