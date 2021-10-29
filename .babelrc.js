a420a5adf4f24cb73f40fe6ad391ba0f1fd1f7bd
rasmusguterstam <rasmus@hedvig.com>
2021-10-29 11:07:49 +0200

    Re-add @babel/plugin-syntax-dynamic-import

--- .babelrc.js
+++ .babelrc.js
@@ -21,9 +21,6 @@ module.exports = (api) => {
       '@emotion',
       'react-hot-loader/babel',
       '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
     ],
     env: {
       test: {
