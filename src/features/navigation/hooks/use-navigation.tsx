cb97f4b4b84f725b6017d3778a8b8da7768e61f6
Maxim <maxim.pletenev@fastdev.se>
2021-12-01 15:15:26 +0500

    Add sub tabs navigation

--- src/features/navigation/hooks/use-navigation.tsx
+++ src/features/navigation/hooks/use-navigation.tsx
@@ -29,6 +29,7 @@ interface FocusItemsType {
       Chat: string
       Tabs: string
       ContractTable: string
+      QuoteTabs: string
     }
   }
   Conversations: {
@@ -74,6 +75,7 @@ export const FocusItems: FocusItemsType = {
       Chat: 'MEMBER_CHAT',
       Tabs: 'MEMBER_TABS',
       ContractTable: 'MEMBER_CONTRACT_TABLE',
+      QuoteTabs: 'MEMBER_QUOTE_TABS',
     },
   },
   Conversations: {
