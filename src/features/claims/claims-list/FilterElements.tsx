00b30cb43790ca0761f2173b4862bb34e1d845bf
Maxim <maxim.pletenev@fastdev.se>
2021-12-03 16:18:12 +0500

    Minor stylish of focused Claims filters

--- src/features/claims/claims-list/FilterElements.tsx
+++ src/features/claims/claims-list/FilterElements.tsx
@@ -8,8 +8,11 @@ import { FilterGroupState, LabelWithPopover } from './filters/ClaimListFilters'
 
 export const Filters = styled(Flex)<{ active: boolean }>`
   width: fit-content;
+
+  border-radius: 0.5rem;
+  padding: ${({ active }) => (active ? '0.1rem 0.5rem' : '0.2rem 0.6rem')};
   border: ${({ active, theme }) =>
-    active ? `2px solid ${theme.accentLight}` : 'none'};
+    active ? `0.1rem solid ${theme.accentLight}` : 'none'};
 `
 
 export const FilterElementStyled = styled.div`
