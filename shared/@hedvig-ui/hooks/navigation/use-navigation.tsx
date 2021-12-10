cef5fd92d0f8e9ee5045c6ca9ed15e033ac27ef1
rasmusguterstam <rasmus@hedvig.com>
2021-12-10 11:03:08 +0100

    Add useNavigation hook to @hedvig-ui

---
+++ shared/@hedvig-ui/hooks/navigation/use-navigation.tsx
@@ -0,0 +1,218 @@
+import {
+  isPressing,
+  Key,
+  Keys,
+} from '@hedvig-ui/hooks/keyboard/use-key-is-pressed'
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+
+interface NavigationContextProps {
+  cursor: string | null
+  setCursor: (focus: string | null) => void
+  registry: Record<string, UseNavigationRegisterOptions>
+  setRegistryItem: (name: string, options: UseNavigationRegisterOptions) => void
+  removeRegistryItem: (name: string) => void
+}
+
+const NavigationContext = createContext<NavigationContextProps>({
+  cursor: null,
+  setCursor: (_: string | null) => void 0,
+  registry: {},
+  setRegistryItem: (_: string, __) => void 0,
+  removeRegistryItem: (_: string) => false,
+})
+
+export const NavigationProvider = ({ children }) => {
+  const [cursor, setCursor] = useState<string | null>(null)
+  const cursorRef = useRef<string | null>(null)
+  const registry = useRef<Record<string, UseNavigationRegisterOptions>>({})
+
+  const handleKeydown = (e) => {
+    if (e.target?.nodeName === 'INPUT' || e.target?.nodeName === 'TEXTAREA') {
+      return
+    }
+
+    Object.keys(registry.current).some((name) => {
+      const options = registry.current[name]
+
+      if (!options?.focus) {
+        return false
+      }
+
+      if (isPressing(e, options.focus)) {
+        setCursor(name)
+        cursorRef.current = name
+        return true
+      }
+
+      return false
+    })
+
+    const target = cursorRef.current
+      ? registry.current[cursorRef.current]
+      : null
+
+    if (!target) {
+      return
+    }
+
+    if (isPressing(e, Keys.Escape)) {
+      setCursor(target.parent ?? null)
+      cursorRef.current = target.parent ?? null
+      return
+    }
+
+    if (isPressing(e, Keys.Enter)) {
+      if (typeof target.resolve === 'string') {
+        setCursor(target.resolve)
+        cursorRef.current = target.resolve
+        return
+      }
+
+      const nextCursor = target.resolve() ?? null
+      setCursor(nextCursor)
+      cursorRef.current = nextCursor
+
+      return
+    }
+
+    e.preventDefault()
+
+    if (isPressing(e, Keys.Up) && target?.neighbors?.up) {
+      const nextCursor =
+        typeof target.neighbors.up === 'function'
+          ? target.neighbors.up()
+          : target.neighbors.up
+
+      setCursor(nextCursor)
+      cursorRef.current = nextCursor
+      return
+    }
+
+    if (isPressing(e, Keys.Down) && target?.neighbors?.down) {
+      const nextCursor =
+        typeof target.neighbors.down === 'function'
+          ? target.neighbors.down()
+          : target.neighbors.down
+
+      setCursor(nextCursor)
+      cursorRef.current = nextCursor
+      return
+    }
+
+    if (isPressing(e, Keys.Left) && target?.neighbors?.left) {
+      const nextCursor =
+        typeof target.neighbors.left === 'function'
+          ? target.neighbors.left()
+          : target.neighbors.left
+
+      setCursor(nextCursor)
+      cursorRef.current = nextCursor
+      return
+    }
+
+    if (isPressing(e, Keys.Right) && target?.neighbors?.right) {
+      const nextCursor =
+        typeof target.neighbors.right === 'function'
+          ? target.neighbors.right()
+          : target.neighbors.right
+
+      setCursor(nextCursor)
+      cursorRef.current = nextCursor
+      return
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeydown)
+    return () => {
+      window.removeEventListener('keydown', handleKeydown)
+    }
+  }, [])
+
+  const handleCursorChange = (value: string | null) => {
+    setCursor(value)
+  }
+
+  const handleSetRegistryItem = (
+    name: string,
+    options: UseNavigationRegisterOptions,
+  ) => {
+    registry.current[name] = options
+  }
+
+  const removeRegistryItem = (name: string) => {
+    delete registry.current[name]
+  }
+
+  return (
+    <NavigationContext.Provider
+      value={{
+        cursor,
+        setCursor: handleCursorChange,
+        registry: registry.current,
+        setRegistryItem: handleSetRegistryItem,
+        removeRegistryItem,
+      }}
+    >
+      {children}
+    </NavigationContext.Provider>
+  )
+}
+
+interface NodeNavigationDirections {
+  up?: string | (() => string)
+  down?: string | (() => string)
+  left?: string | (() => string)
+  right?: string | (() => string)
+}
+
+interface UseNavigationRegisterOptions {
+  focus?: Key
+  resolve: string | (() => string | void)
+  parent?: string
+  neighbors?: NodeNavigationDirections
+}
+
+export const useNavigation = () => {
+  const { cursor, registry, setRegistryItem, removeRegistryItem } = useContext(
+    NavigationContext,
+  )
+  const localItems = useRef<Record<string, UseNavigationRegisterOptions>>({})
+
+  const registerItem = (
+    name: string,
+    options: UseNavigationRegisterOptions,
+  ) => {
+    setRegistryItem(name, options)
+  }
+
+  const itemExists = (name: string) => !!registry[name]
+
+  useEffect(() => {
+    return () => {
+      Object.keys(localItems).forEach((name) => {
+        removeRegistryItem(name)
+      })
+    }
+  })
+
+  return {
+    register: (name: string, options: UseNavigationRegisterOptions) => {
+      if (!itemExists(name)) {
+        registerItem(name, options)
+      }
+
+      if (cursor !== name) {
+        return {}
+      }
+
+      return { style: { border: '2px solid blue' } }
+    },
+  }
+}
