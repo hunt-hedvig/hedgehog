6ff8dc9aa61d22fe945c0b0adade2adf6e3ba7a6
Maxim <maxim.pletenev@fastdev.se>
2021-12-17 10:26:55 +0500

    Revert "Fix rerender on key pressing in member page"
    This reverts commit 7192158341adb29b3f60c3aed7c027cc7c69124b.

--- src/pages/members/MemberPage.tsx
+++ src/pages/members/MemberPage.tsx
@@ -1,7 +1,11 @@
 import { useTitle } from '@hedvig-ui/hooks/use-title'
 import { MemberTabs } from 'features/member'
 import { useGetMemberInfo } from 'features/member/tabs/member-tab/hooks/use-get-member-info'
-import React from 'react'
+import {
+  FocusItems,
+  useNavigation,
+} from 'features/navigation/hooks/use-navigation'
+import React, { useEffect } from 'react'
 import { RouteComponentProps } from 'react-router'
 import { Page } from 'pages/routes'
 
@@ -15,11 +19,27 @@ const MemberPage: Page<
 
   useTitle(member ? `${member?.firstName} ${member?.lastName}` : 'Loading...')
 
+  const { focus, setFocus } = useNavigation()
+
+  useEffect(() => {
+    if (!focus) {
+      setFocus(FocusItems.Member.name)
+    }
+  }, [focus])
+
   if (!member) {
     return null
   }
 
-  return <MemberTabs {...props} member={member} />
+  return (
+    <MemberTabs
+      {...props}
+      member={member}
+      navigationAvailable={focus === FocusItems.Member.name}
+      setFocus={(value: string | null) => setFocus(value)}
+      focus={focus}
+    />
+  )
 }
 
 export default MemberPage
