bc77006b4c7785fc42331579f811b114bd0aa9d7
rasmusguterstam <rasmus@hedvig.com>
2022-01-12 18:28:20 +0100

    Remove prop-types

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+  },
+  ignorePatterns: ['/src/types/**/*']
+}
