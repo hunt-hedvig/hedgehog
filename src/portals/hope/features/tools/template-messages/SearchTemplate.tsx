fa486c118d8d2935f9228c9374f278083dd7004c
Maxim <maxim.pletenev@fastdev.se>
2022-01-18 11:43:46 +0500

    Add some test values

--- src/portals/hope/features/tools/template-messages/SearchTemplate.tsx
+++ src/portals/hope/features/tools/template-messages/SearchTemplate.tsx
@@ -1,14 +1,132 @@
-import React from 'react'
-import { TemplateMessage } from 'portals/hope/pages/tools/TemplateMessagesPage'
+import React, { useEffect, useState } from 'react'
+import styled from '@emotion/styled'
+import {
+  Languages,
+  TemplateMessage,
+} from 'portals/hope/pages/tools/TemplateMessagesPage'
+import { Input } from '@hedvig-ui'
+import { SearchIcon as InputIcon } from '../../members-search/styles'
+import { FileText } from 'react-bootstrap-icons'
+import { denmarkTemplates, norwayTemplates, swedenTemplates } from './templates'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Content = styled.div`
+  height: 100%;
+  max-height: 35rem;
+  overflow-y: auto;
+  border: 1px solid ${({ theme }) => theme.border};
+  border-top: none;
+`
+
+const SearchIcon = styled(InputIcon)`
+  width: 1rem;
+  height: 1rem;
+`
+
+const SearchInput = styled(Input)`
+  border-radius: unset;
+`
+
+const Item = styled.div`
+  padding: 0.5rem;
+
+  display: flex;
+  align-items: center;
+
+  border-bottom: 1px solid ${({ theme }) => theme.border};
+
+  cursor: pointer;
+
+  & span {
+    margin-left: 0.5rem;
+    font-size: 0.875rem;
+    padding-top: 1px;
+  }
+
+  &:hover {
+    background-color: ${({ theme }) => theme.accentLighter};
+  }
+`
+
+const getTemplates = (language: Languages) =>
+  language === Languages.Denmark
+    ? denmarkTemplates
+    : language === Languages.Norway
+    ? norwayTemplates
+    : swedenTemplates
 
 export const SearchTemplate: React.FC<{
-  language: 'sweden' | 'denmark' | 'norway'
+  language: Languages
   selected: TemplateMessage | null
   onSelect: (template: TemplateMessage) => void
 }> = ({ language }) => {
+  const [currentTemplates, setCurrentTemplates] = useState(
+    getTemplates(language),
+  )
+  const [query, setQuery] = useState<string>()
+
+  useEffect(() => {
+    if (query) {
+      setCurrentTemplates((prev) =>
+        prev.filter((template) => template.name.includes(query)),
+      )
+    } else {
+      setCurrentTemplates(getTemplates(language))
+    }
+  }, [query])
+
+  useEffect(() => {
+    if (!query) {
+      setCurrentTemplates(getTemplates(language))
+      return
+    }
+
+    const templates = getTemplates(language)
+    setCurrentTemplates(
+      templates.filter((template) => template.name.includes(query)),
+    )
+  }, [language])
+
+  const selectHandler = (id: string) => {
+    console.log(id)
+  }
+
   return (
-    <div>
-      <h1>Search Template ({language})</h1>
-    </div>
+    <Container>
+      <SearchInput
+        style={{ borderRadius: 'unset' }}
+        onChange={({ target: { value } }) => {
+          setQuery(value)
+        }}
+        icon={<SearchIcon muted={!query} />}
+        placeholder="Search Template"
+        id="query"
+        value={query}
+        type="search"
+        autoFocus
+      />
+      <Content>
+        {currentTemplates.map((template) => (
+          <TemplateItem
+            id={template.id}
+            name={template.name}
+            onSelect={selectHandler}
+          />
+        ))}
+      </Content>
+    </Container>
+  )
+}
+
+const TemplateItem = ({ id, name, onSelect }) => {
+  return (
+    <Item onClick={() => onSelect(id)}>
+      <FileText />
+      <span>{name}</span>
+    </Item>
   )
 }
---
+++ src/portals/hope/features/tools/template-messages/templates.tsx
@@ -0,0 +1,224 @@
+export const swedenTemplates = [
+  {
+    id: '1',
+    name: 'Sweden Template #1',
+  },
+  {
+    id: '2',
+    name: 'Sweden Template #2',
+  },
+  {
+    id: '3',
+    name: 'Sweden Template #3',
+  },
+  {
+    id: '4',
+    name: 'Sweden Template #4',
+  },
+  {
+    id: '5',
+    name: 'Sweden Template #5',
+  },
+  {
+    id: '6',
+    name: 'Sweden Template #6',
+  },
+  {
+    id: '7',
+    name: 'Sweden Template #7',
+  },
+  {
+    id: '8',
+    name: 'Sweden Template #8',
+  },
+  {
+    id: '9',
+    name: 'Sweden Template #9',
+  },
+  {
+    id: '10',
+    name: 'Sweden Template #10',
+  },
+  {
+    id: '11',
+    name: 'Sweden Template #11',
+  },
+  {
+    id: '12',
+    name: 'Sweden Template #12',
+  },
+  {
+    id: '13',
+    name: 'Sweden Template #13',
+  },
+  {
+    id: '14',
+    name: 'Sweden Template #14',
+  },
+  {
+    id: '15',
+    name: 'Sweden Template #15',
+  },
+  {
+    id: '16',
+    name: 'Sweden Template #16',
+  },
+  {
+    id: '17',
+    name: 'Sweden Template #17',
+  },
+  {
+    id: '18',
+    name: 'Sweden Template #18',
+  },
+]
+
+export const denmarkTemplates = [
+  {
+    id: '19',
+    name: 'Denmark Template #1',
+  },
+  {
+    id: '20',
+    name: 'Denmark Template #2',
+  },
+  {
+    id: '21',
+    name: 'Denmark Template #3',
+  },
+  {
+    id: '22',
+    name: 'Denmark Template #4',
+  },
+  {
+    id: '23',
+    name: 'Denmark Template #5',
+  },
+  {
+    id: '24',
+    name: 'Denmark Template #6',
+  },
+  {
+    id: '25',
+    name: 'Denmark Template #7',
+  },
+  {
+    id: '26',
+    name: 'Denmark Template #8',
+  },
+  {
+    id: '27',
+    name: 'Denmark Template #9',
+  },
+  {
+    id: '28',
+    name: 'Denmark Template #10',
+  },
+  {
+    id: '29',
+    name: 'Denmark Template #11',
+  },
+  {
+    id: '30',
+    name: 'Denmark Template #12',
+  },
+  {
+    id: '31',
+    name: 'Denmark Template #13',
+  },
+  {
+    id: '32',
+    name: 'Denmark Template #14',
+  },
+  {
+    id: '33',
+    name: 'Denmark Template #15',
+  },
+  {
+    id: '34',
+    name: 'Denmark Template #16',
+  },
+  {
+    id: '35',
+    name: 'Denmark Template #17',
+  },
+  {
+    id: '36',
+    name: 'Denmark Template #18',
+  },
+]
+
+export const norwayTemplates = [
+  {
+    id: '37',
+    name: 'Norway Template #1',
+  },
+  {
+    id: '38',
+    name: 'Norway Template #2',
+  },
+  {
+    id: '39',
+    name: 'Norway Template #3',
+  },
+  {
+    id: '40',
+    name: 'Norway Template #4',
+  },
+  {
+    id: '41',
+    name: 'Norway Template #5',
+  },
+  {
+    id: '42',
+    name: 'Norway Template #6',
+  },
+  {
+    id: '43',
+    name: 'Norway Template #7',
+  },
+  {
+    id: '44',
+    name: 'Norway Template #8',
+  },
+  {
+    id: '45',
+    name: 'Norway Template #9',
+  },
+  {
+    id: '46',
+    name: 'Norway Template #10',
+  },
+  {
+    id: '47',
+    name: 'Norway Template #11',
+  },
+  {
+    id: '48',
+    name: 'Norway Template #12',
+  },
+  {
+    id: '49',
+    name: 'Norway Template #13',
+  },
+  {
+    id: '50',
+    name: 'Norway Template #14',
+  },
+  {
+    id: '51',
+    name: 'Norway Template #15',
+  },
+  {
+    id: '52',
+    name: 'Norway Template #16',
+  },
+  {
+    id: '53',
+    name: 'Norway Template #17',
+  },
+  {
+    id: '54',
+    name: 'Norway Template #18',
+  },
+]
