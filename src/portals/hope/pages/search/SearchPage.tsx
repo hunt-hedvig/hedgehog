74aad616e7d5fa7a4ef3009518f1c9121f0710a6
rasmusguterstam <rasmus@hedvig.com>
2022-02-24 09:33:19 +0100

    Add "show more" button to search

--- src/portals/hope/pages/search/SearchPage.tsx
+++ src/portals/hope/pages/search/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { Page } from 'portals/hope/pages/routes'
 import {
+  Button,
   Flex,
   Input,
   Loadable,
@@ -67,8 +68,8 @@ const SearchInput = styled(Input)`
 `
 
 gql`
-  query Search($query: String!) {
-    search(query: $query) {
+  query Search($query: String!, $from: Int, $size: Int) {
+    search(query: $query, from: $from, size: $size) {
       memberId
       firstName
       lastName
@@ -84,7 +85,7 @@ const SearchPage: Page = () => {
   const history = useHistory()
   const [result, setResult] = useState<SearchQuery['search']>([])
   const [query, setQuery] = useState('')
-  const [search, { data, loading }] = useSearchLazyQuery({
+  const [search, { loading }] = useSearchLazyQuery({
     variables: { query },
   })
 
@@ -92,142 +93,169 @@ const SearchPage: Page = () => {
 
   useEffect(() => {
     if (debouncedQuery.length >= 3 && !loading) {
-      search({ variables: { query: debouncedQuery } })
+      search({ variables: { query: debouncedQuery } }).then(({ data }) => {
+        if (!data) {
+          return
+        }
+
+        setResult(data.search)
+      })
     }
   }, [debouncedQuery])
 
-  useEffect(() => {
-    if (data) {
-      setResult(data.search)
-    }
-  }, [data])
-
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault()
-        search({ variables: { query } })
-      }}
-    >
-      <SearchInput
-        onChange={(e) => setQuery(e.currentTarget.value)}
-        value={query}
-        size="large"
-        muted={!query}
-        placeholder="What are you looking for?"
-        icon={<SearchIcon muted={!query} />}
-        loading={loading}
-        autoFocus
-      />
-      <Spacing top />
-      {result.length !== 0 && (
-        <Table>
-          <TableHeader>
-            <TableHeaderColumn>Member</TableHeaderColumn>
-            <TableHeaderColumn>Signed Up</TableHeaderColumn>
-            <TableHeaderColumn>First Master Inception</TableHeaderColumn>
-            <TableHeaderColumn>Last Termination Date</TableHeaderColumn>
-            <TableHeaderColumn>Contracts</TableHeaderColumn>
-          </TableHeader>
-          <TableBody>
-            {result.map((member, index) => (
-              <>
-                <TableRow
-                  index={index}
-                  length={result.length}
-                  key={member.memberId}
-                  tabIndex={0}
-                  onClick={() =>
-                    history.push(`/members/${member.memberId}/contracts`)
-                  }
-                >
-                  <TableColumn style={{ verticalAlign: 'top' }}>
-                    <Flex direction="column">
-                      {member.firstName} {member.lastName}
-                      <Flex>
-                        <TableColumnSubtext>
-                          {member.memberId}
-                        </TableColumnSubtext>
-                        <MemberAgeWrapper>
-                          <Loadable loading={true}>
-                            <span>99 years</span>
-                          </Loadable>
-                        </MemberAgeWrapper>
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          search({ variables: { query } })
+        }}
+      >
+        <SearchInput
+          onChange={(e) => setQuery(e.currentTarget.value)}
+          value={query}
+          size="large"
+          muted={!query}
+          placeholder="What are you looking for?"
+          icon={<SearchIcon muted={!query} />}
+          loading={loading}
+          autoFocus
+        />
+        <Spacing top />
+        {result.length !== 0 && (
+          <Table>
+            <TableHeader>
+              <TableHeaderColumn>Member</TableHeaderColumn>
+              <TableHeaderColumn>Signed Up</TableHeaderColumn>
+              <TableHeaderColumn>First Master Inception</TableHeaderColumn>
+              <TableHeaderColumn>Last Termination Date</TableHeaderColumn>
+              <TableHeaderColumn>Contracts</TableHeaderColumn>
+            </TableHeader>
+            <TableBody>
+              {result.map((member, index) => (
+                <>
+                  <TableRow
+                    index={index}
+                    length={result.length}
+                    key={member.memberId}
+                    tabIndex={0}
+                    onClick={() =>
+                      history.push(`/members/${member.memberId}/contracts`)
+                    }
+                  >
+                    <TableColumn style={{ verticalAlign: 'top' }}>
+                      <Flex direction="column">
+                        {member.firstName && member.lastName ? (
+                          `${member.firstName} ${member.lastName}`
+                        ) : (
+                          <Placeholder>Not available</Placeholder>
+                        )}
+                        <Flex>
+                          <TableColumnSubtext>
+                            {member.memberId}
+                          </TableColumnSubtext>
+                          <MemberAgeWrapper>
+                            <Loadable loading={true}>
+                              <span>99 years</span>
+                            </Loadable>
+                          </MemberAgeWrapper>
+                        </Flex>
                       </Flex>
-                    </Flex>
-                  </TableColumn>
+                    </TableColumn>
 
-                  <TableColumn style={{ verticalAlign: 'top' }}>
-                    <Flex direction="column">
+                    <TableColumn style={{ verticalAlign: 'top' }}>
+                      <Flex direction="column">
+                        <Loadable loading={true}>
+                          <span>Not specified</span>
+                        </Loadable>
+                        <Loadable loading={true}>
+                          <TableColumnSubtext>Not specified</TableColumnSubtext>
+                        </Loadable>
+                      </Flex>
+                    </TableColumn>
+                    <TableColumn style={{ verticalAlign: 'top' }}>
                       <Loadable loading={true}>
                         <span>Not specified</span>
                       </Loadable>
+                    </TableColumn>
+                    <TableColumn style={{ verticalAlign: 'top' }}>
                       <Loadable loading={true}>
-                        <TableColumnSubtext>Not specified</TableColumnSubtext>
+                        <Placeholder>Not specified</Placeholder>
                       </Loadable>
-                    </Flex>
-                  </TableColumn>
-                  <TableColumn style={{ verticalAlign: 'top' }}>
-                    <Loadable loading={true}>
-                      <span>Not specified</span>
-                    </Loadable>
-                  </TableColumn>
-                  <TableColumn style={{ verticalAlign: 'top' }}>
-                    <Loadable loading={true}>
-                      <Placeholder>Not specified</Placeholder>
-                    </Loadable>
-                  </TableColumn>
-                  <TableColumn style={{ verticalAlign: 'top' }}>
-                    <div
-                      style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                      }}
-                    >
-                      <Loadable loading={true}>
-                        <ContractCountWrapper>
-                          <ContractCountNumber variation="placeholderColor">
-                            0
-                          </ContractCountNumber>
-                          <ContractCountLabel>Pending</ContractCountLabel>
-                        </ContractCountWrapper>
-                      </Loadable>
-                      <Loadable loading={true}>
-                        <ContractCountWrapper>
-                          <ContractCountNumber variation="placeholderColor">
-                            0
-                          </ContractCountNumber>
-                          <ContractCountLabel>
-                            Active in future
-                          </ContractCountLabel>
-                        </ContractCountWrapper>
-                      </Loadable>
-                      <Loadable loading={true}>
-                        <ContractCountWrapper>
-                          <ContractCountNumber variation="placeholderColor">
-                            0
-                          </ContractCountNumber>
-                          <ContractCountLabel>Active</ContractCountLabel>
-                        </ContractCountWrapper>
-                      </Loadable>
-                      <Loadable loading={true}>
-                        <ContractCountWrapper>
-                          <ContractCountNumber variation="placeholderColor">
-                            0
-                          </ContractCountNumber>
-                          <ContractCountLabel>Terminated</ContractCountLabel>
-                        </ContractCountWrapper>
-                      </Loadable>
-                    </div>
-                  </TableColumn>
-                </TableRow>
-              </>
-            ))}
-          </TableBody>
-        </Table>
-      )}
-    </form>
+                    </TableColumn>
+                    <TableColumn style={{ verticalAlign: 'top' }}>
+                      <div
+                        style={{
+                          display: 'flex',
+                          flexDirection: 'row',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <Loadable loading={true}>
+                          <ContractCountWrapper>
+                            <ContractCountNumber variation="placeholderColor">
+                              0
+                            </ContractCountNumber>
+                            <ContractCountLabel>Pending</ContractCountLabel>
+                          </ContractCountWrapper>
+                        </Loadable>
+                        <Loadable loading={true}>
+                          <ContractCountWrapper>
+                            <ContractCountNumber variation="placeholderColor">
+                              0
+                            </ContractCountNumber>
+                            <ContractCountLabel>
+                              Active in future
+                            </ContractCountLabel>
+                          </ContractCountWrapper>
+                        </Loadable>
+                        <Loadable loading={true}>
+                          <ContractCountWrapper>
+                            <ContractCountNumber variation="placeholderColor">
+                              0
+                            </ContractCountNumber>
+                            <ContractCountLabel>Active</ContractCountLabel>
+                          </ContractCountWrapper>
+                        </Loadable>
+                        <Loadable loading={true}>
+                          <ContractCountWrapper>
+                            <ContractCountNumber variation="placeholderColor">
+                              0
+                            </ContractCountNumber>
+                            <ContractCountLabel>Terminated</ContractCountLabel>
+                          </ContractCountWrapper>
+                        </Loadable>
+                      </div>
+                    </TableColumn>
+                  </TableRow>
+                </>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </form>
+      <Spacing top="medium" />
+      <Flex justify="center">
+        {result.length !== 0 && (
+          <Button
+            variant="tertiary"
+            onClick={() => {
+              search({
+                variables: { query: debouncedQuery, from: result.length },
+              }).then(({ data }) => {
+                if (!data) {
+                  return
+                }
+
+                setResult([...result, ...data.search])
+              })
+            }}
+          >
+            Show more
+          </Button>
+        )}
+      </Flex>
+    </>
   )
 }
 
