9b9ee5b5954ab0322f9b1e90e6b8fdd27b544f29
rasmusguterstam <rasmus@hedvig.com>
2022-03-11 13:57:15 +0100

    Debugging in staging

--- src/portals/hope/features/search/members/components/MemberHitRow.tsx
+++ src/portals/hope/features/search/members/components/MemberHitRow.tsx
@@ -15,7 +15,7 @@ import {
   TableRow,
 } from '@hedvig-ui'
 import { getMemberFlag, MemberAge } from 'portals/hope/features/member/utils'
-import parse from 'html-react-parser'
+//import parse from 'html-react-parser'
 import formatDate from 'date-fns/format'
 import { parseISO } from 'date-fns'
 import {
@@ -92,12 +92,14 @@ const convertTagText = (text: string) => {
 const SearchHitTag: React.FC<{
   highlight: ArrayElement<SearchQuery['search'][0]['highlights']>
 }> = ({ highlight }) => {
+  /*
   const contents = parse(
     [...new Set(highlight.values)]
       .reduce<string>((acc, value) => acc + value + '<br/>', '')
       .replaceAll('<em>', '<b>')
       .replaceAll('</em>', '</b>'),
   )
+   */
 
   return (
     <Tag>
@@ -107,7 +109,7 @@ const SearchHitTag: React.FC<{
           minWidth: '15rem',
           overflowWrap: 'break-word',
         }}
-        contents={contents}
+        contents={''}
       >
         {convertTagText(highlight.field).replaceAll('.', ', ')}
       </Popover>
