3045b99c8a9a8ccabc0cb41974af71bf80b2e855
rasmusguterstam <rasmus@hedvig.com>
2022-03-14 14:09:53 +0100

    Add query param for search query

--- src/portals/hope/pages/search/SearchPage.tsx
+++ src/portals/hope/pages/search/SearchPage.tsx
@@ -1,9 +1,15 @@
 import { Page } from 'portals/hope/pages/routes'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { MemberSearch } from 'portals/hope/features/search/members/MemberSearch'
-import { RouteComponentProps } from 'react-router'
+import { RouteComponentProps, useLocation } from 'react-router'
 import { SearchCategory } from 'portals/hope/features/search/components/SearchCategoryButtons'
 import { QuoteSearch } from 'portals/hope/features/search/quotes/QuoteSearch'
+
+const useQueryParams = () => {
+  const { search } = useLocation()
+
+  return useMemo(() => new URLSearchParams(search), [search])
+}
 
 const SearchPage: Page<
   RouteComponentProps<{
@@ -11,12 +17,13 @@ const SearchPage: Page<
   }>
 > = ({ match }) => {
   const category = match?.params?.category ?? 'members'
+  const queryParams = useQueryParams()
 
   switch (category) {
     case 'quotes':
-      return <QuoteSearch />
+      return <QuoteSearch query={queryParams.get('query')} />
     case 'members':
-      return <MemberSearch />
+      return <MemberSearch query={queryParams.get('query')} />
   }
 }
 
