5597a347aa6732e0bd9ad4bfc43f2a1aeec90858
rasmusguterstam <rasmus@hedvig.com>
2022-03-14 14:40:20 +0100

    Rever theme change

--- src/portals/hope/pages/search/SearchPage.tsx
+++ src/portals/hope/pages/search/SearchPage.tsx
@@ -1,11 +1,9 @@
 import { Page } from 'portals/hope/pages/routes'
 import React, { useMemo } from 'react'
 import { MemberSearch } from 'portals/hope/features/search/members/MemberSearch'
-import { RouteComponentProps, useHistory, useLocation } from 'react-router'
+import { RouteComponentProps, useLocation } from 'react-router'
 import { SearchCategory } from 'portals/hope/features/search/components/SearchCategoryButtons'
 import { QuoteSearch } from 'portals/hope/features/search/quotes/QuoteSearch'
-import { useCommandLine } from 'portals/hope/features/commands/use-command-line'
-import { Keys } from '@hedvig-ui/hooks/keyboard/use-key-is-pressed'
 
 const useQueryParams = () => {
   const { search } = useLocation()
@@ -18,31 +16,8 @@ const SearchPage: Page<
     category?: SearchCategory
   }>
 > = ({ match }) => {
-  const history = useHistory()
   const category = match?.params?.category ?? 'members'
   const queryParams = useQueryParams()
-
-  const { registerActions } = useCommandLine()
-
-  registerActions([
-    {
-      label: `Search members`,
-      keys: [Keys.Control, Keys.M],
-      onResolve: () => {
-        history.push(`/search/members?query=${queryParams.get('query')}`)
-      },
-    },
-  ])
-
-  registerActions([
-    {
-      label: `Search quotes`,
-      keys: [Keys.Control, Keys.Q],
-      onResolve: () => {
-        history.push(`/search/quotes?query=${queryParams.get('query')}`)
-      },
-    },
-  ])
 
   switch (category) {
     case 'quotes':
