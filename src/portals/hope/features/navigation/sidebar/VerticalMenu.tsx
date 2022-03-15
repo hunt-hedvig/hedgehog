935b0f7aa44ab3d90bfed1821c27e9dd196c25a0
rasmusguterstam <rasmus@hedvig.com>
2022-03-15 09:25:55 +0100

    Refactor check-out card

--- src/portals/hope/features/navigation/sidebar/VerticalMenu.tsx
+++ src/portals/hope/features/navigation/sidebar/VerticalMenu.tsx
@@ -22,10 +22,7 @@ import { Logo, LogoIcon } from './elements'
 import { ExternalMenuItem, MenuItem } from './MenuItem'
 import { Keys } from '@hedvig-ui/hooks/keyboard/use-key-is-pressed'
 import { useNavigation } from '@hedvig-ui/hooks/navigation/use-navigation'
-import { Button, Flex } from '@hedvig-ui'
-import chroma from 'chroma-js'
-import { useCheckInOut } from 'portals/hope/features/tasks/hooks/use-check-in-out'
-import toast from 'react-hot-toast'
+import { CheckedInCard } from 'portals/hope/features/navigation/sidebar/CheckedInCard'
 
 const Wrapper = styled.div<{ collapsed: boolean }>`
   display: inline-block;
@@ -59,22 +56,22 @@ const Wrapper = styled.div<{ collapsed: boolean }>`
   }
 `
 
-const InnerWrapper = styled('div')({
-  position: 'sticky',
-  display: 'flex',
-  flexDirection: 'column',
-  top: 0,
-  height: '100vh',
-  overflowY: 'scroll',
-  width: '100%',
-  margin: 'auto',
+const InnerWrapper = styled.div`
+  position: sticky;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  height: 100vh;
+  overflow-y: scroll;
+  width: 100%;
+  margin: auto;
 
-  '&::-webkit-scrollbar, &::-moz-scrollbar': {
-    appearance: 'none',
-    width: 0,
-    display: 'none',
-  },
-})
+  &::-webkit-scrollbar {
+    appearance: none;
+    width: 0;
+    display: none;
+  }
+`
 
 const Header = styled('div')({
   position: 'relative',
@@ -148,8 +145,6 @@ const routes = {
 }
 
 export const VerticalMenu: React.FC = () => {
-  const { checkOut, checkedIn } = useCheckInOut()
-
   const history = useHistory()
   const { settings } = useMe()
   const { pathname } = useLocation()
@@ -337,52 +332,7 @@ export const VerticalMenu: React.FC = () => {
                   />
                 )
               })}
-              {checkedIn && !isCollapsed && !shouldAlwaysCollapse && (
-                <CheckedInWrapper>
-                  <Flex direction="column">
-                    <Flex align="center" justify="space-between">
-                      <div>
-                        <h4>You are checked-in </h4>
-                      </div>
-                      <div style={{ marginTop: '-0.75rem', width: '1rem' }}>
-                        <div
-                          style={{
-                            position: 'relative',
-                          }}
-                        >
-                          <div
-                            className="bouncing-orb"
-                            style={{ animationDelay: '0s' }}
-                          />
-                          <div
-                            className="bouncing-orb"
-                            style={{ animationDelay: '1s' }}
-                          />
-                          <div
-                            className="bouncing-orb"
-                            style={{ animationDelay: '2s' }}
-                          />
-                          <div
-                            className="bouncing-orb"
-                            style={{ animationDelay: '3s' }}
-                          />
-                        </div>
-                      </div>
-                    </Flex>
-                    <Flex justify="space-between" style={{ marginTop: '1rem' }}>
-                      <Button
-                        variant="secondary"
-                        onClick={() => {
-                          toast.success('Checked out')
-                          checkOut()
-                        }}
-                      >
-                        Check out
-                      </Button>
-                    </Flex>
-                  </Flex>
-                </CheckedInWrapper>
-              )}
+              {!isCollapsed && !shouldAlwaysCollapse && <CheckedInCard />}
             </Menu>
           </InnerWrapper>
         </Wrapper>
@@ -390,52 +340,3 @@ export const VerticalMenu: React.FC = () => {
     </MediaQuery>
   )
 }
-
-const CheckedInWrapper = styled.div`
-  padding: 1.2rem;
-  background-color: ${({ theme }) => theme.accent};
-  border-radius: 0.25rem;
-
-  h4 {
-    font-size: 1.3rem;
-  }
-
-  p {
-    font-size: 1rem;
-    color: ${({ theme }) => chroma(theme.accentContrast).alpha(0.6).hex()};
-  }
-
-  .n-questions {
-    background-color: ${({ theme }) =>
-      chroma(theme.accentContrast).alpha(0.15).hex()};
-    color: ${({ theme }) => theme.accentContrast};
-    border-radius: 0.25rem;
-    padding: 0.1rem 0.2rem;
-  }
-
-  @keyframes scaleIn {
-    from {
-      transform: scale(0.5, 0.5);
-      opacity: 0.5;
-    }
-    to {
-      transform: scale(2.5, 2.5);
-      opacity: 0;
-    }
-  }
-
-  .bouncing-orb {
-    position: absolute;
-    top: 0;
-    left: 0;
-
-    width: 0.75rem;
-    height: 0.75rem;
-
-    border-radius: 50%;
-
-    animation: scaleIn 4s infinite cubic-bezier(0.36, 0.11, 0.89, 0.32);
-
-    background-color: ${({ theme }) => theme.accentContrast};
-  }
-`
