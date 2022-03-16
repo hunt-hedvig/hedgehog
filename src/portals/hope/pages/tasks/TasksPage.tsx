f8f6ea5bdd1f9fcb4a6d8e6336723284ec8c08ae
rasmusguterstam <rasmus@hedvig.com>
2022-03-16 10:40:01 +0100

    Add topbar to tasks

--- src/portals/hope/pages/tasks/TasksPage.tsx
+++ src/portals/hope/pages/tasks/TasksPage.tsx
@@ -2,6 +2,7 @@ import { Page } from 'portals/hope/pages/routes'
 import styled from '@emotion/styled'
 import React from 'react'
 import { Flex } from '@hedvig-ui'
+import chroma from 'chroma-js'
 
 const TaskNavigationWrapper = styled.div`
   height: 100%;
@@ -16,10 +17,54 @@ const TaskChatWrapper = styled.div`
   min-width: 37rem;
 `
 
+const TopBar = styled.div`
+  display: flex;
+  border-bottom: 1px solid
+    ${({ theme }) => chroma(theme.semiStrongForeground).brighten(3).hex()};
+  width: 100%;
+`
+
+const TopBarItem = styled.button<{ selected?: boolean }>`
+  background-color: transparent;
+
+  border: none;
+  cursor: pointer;
+
+  display: flex;
+  align-items: center;
+  padding: 1.8rem 2rem;
+
+  color: ${({ theme, selected }) =>
+    selected
+      ? undefined
+      : chroma(theme.semiStrongForeground).brighten(2).hex()};
+
+  transition: color 200ms;
+  :hover {
+    color: ${({ theme }) => chroma(theme.semiStrongForeground).alpha(4).hex()};
+  }
+
+  .count {
+    color: ${({ theme }) =>
+      chroma(theme.semiStrongForeground).brighten(2).hex()};
+    display: inline-block;
+    font-size: 1.1rem;
+    margin-left: 1rem;
+    margin-top: 0.1rem;
+  }
+`
+
 const TasksPage: Page = () => {
   return (
     <Flex style={{ marginTop: '-4rem', marginLeft: '-4rem' }}>
-      <TaskNavigationWrapper />
+      <TaskNavigationWrapper>
+        <TopBar>
+          <TopBarItem selected={true}>
+            Incoming questions
+            <div className="count">10</div>
+          </TopBarItem>
+        </TopBar>
+      </TaskNavigationWrapper>
       <TaskChatWrapper />
     </Flex>
   )
