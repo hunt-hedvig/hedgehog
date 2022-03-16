a7a4963a75bb3c6dc2679bf234db1b91508024fe
rasmusguterstam <rasmus@hedvig.com>
2022-03-16 17:41:07 +0100

    Actually filter question groups

--- src/portals/hope/pages/tasks/TasksPage.tsx
+++ src/portals/hope/pages/tasks/TasksPage.tsx
@@ -1,14 +1,20 @@
 import { Page } from 'portals/hope/pages/routes'
 import styled from '@emotion/styled'
 import React, { useState } from 'react'
-import { Flex } from '@hedvig-ui'
+import { Flex, Modal } from '@hedvig-ui'
 import chroma from 'chroma-js'
-import { CaretDownFill } from 'react-bootstrap-icons'
 import { useQuestionGroups } from 'portals/hope/features/questions/hooks/use-question-groups'
 import { Question, QuestionGroup } from 'types/generated/graphql'
 import { formatDistanceToNowStrict, parseISO } from 'date-fns'
 import { MessagesList } from 'portals/hope/features/member/messages/MessagesList'
 import { TaskChatInput } from 'portals/hope/features/tasks/components/TaskChatInput'
+import { useSelectedFilters } from 'portals/hope/features/questions/hooks/use-selected-filters'
+import { FilterSelect } from 'portals/hope/features/questions/FilterSelect'
+import {
+  doMarketFilter,
+  doMemberGroupFilter,
+} from 'portals/hope/features/questions/utils'
+import { useNumberMemberGroups } from 'portals/hope/features/user/hooks/use-number-member-groups'
 
 const TaskNavigationWrapper = styled.div`
   height: 100%;
@@ -157,6 +163,30 @@ const Container = styled(Flex)`
   margin-left: -4rem;
 `
 
+const FilterModal = styled(Modal)`
+  width: 50rem;
+
+  padding: 1.5rem 1.5rem 5rem;
+
+  h4 {
+    font-size: 1.4rem;
+  }
+
+  p {
+    font-size: 1rem;
+    color: ${({ theme }) => theme.semiStrongForeground};
+  }
+
+  .tip {
+    margin-top: 0.5rem;
+    margin-bottom: -0.5rem;
+    font-size: 0.8rem;
+    color: ${({ theme }) =>
+      chroma(theme.semiStrongForeground).brighten(1).hex()};
+    text-align: center;
+  }
+`
+
 const getQuestionBody = (question: Question) => {
   try {
     return JSON.parse(question.messageJsonString).body
@@ -166,93 +196,139 @@ const getQuestionBody = (question: Question) => {
 }
 
 const TasksPage: Page = () => {
+  const { numberMemberGroups } = useNumberMemberGroups()
+  const { selectedFilters, toggleFilter } = useSelectedFilters()
+
+  const [showFilters, setShowFilters] = useState(false)
+
   const [isLarge, setIsLarge] = useState(false)
   const [selectedQuestionGroup, setSelectedQuestionGroup] =
     useState<QuestionGroup | null>(null)
   const [questionGroups] = useQuestionGroups()
 
-  return (
-    <Container>
-      <TaskNavigationWrapper>
-        <TopBar>
-          <TopBarItem selected={true}>
-            Incoming questions
-            <div className="count">10</div>
-          </TopBarItem>
-        </TopBar>
-        <FilterBar>
-          <FilterBarItem>
-            All questions <CaretDownFill className="icon" />
-          </FilterBarItem>
-        </FilterBar>
-        <ListContainer>
-          {questionGroups.map((group) => {
-            const previewQuestion = group?.questions?.slice(-1)[0] ?? ''
-            const preview = getQuestionBody(previewQuestion)
+  const groups =
+    selectedFilters.length > 0
+      ? questionGroups
+          .filter(doMemberGroupFilter(numberMemberGroups)(selectedFilters))
+          .filter(doMarketFilter(selectedFilters))
+      : questionGroups
 
-            return (
-              <ListItem
-                key={group.id}
-                onClick={() => setSelectedQuestionGroup(group)}
-                selected={group.memberId === selectedQuestionGroup?.memberId}
-              >
-                <span className="name">
-                  {(group?.firstName ?? '') + ' ' + (group?.lastName ?? '')}
-                </span>
-                <span className="preview">{preview.text}</span>
-                <div className="options">
-                  {formatDistanceToNowStrict(
-                    parseISO(previewQuestion.timestamp),
-                    {
-                      addSuffix: true,
-                    },
-                  )}
-                </div>
-              </ListItem>
-            )
-          })}
-        </ListContainer>
-      </TaskNavigationWrapper>
-      <TaskChatWrapper>
-        {selectedQuestionGroup && (
-          <>
-            <div
-              style={{
-                height: isLarge ? 'calc(100% - 27rem)' : 'calc(100% - 15rem)',
-                transition: 'height 200ms',
-              }}
-            >
-              <MessagesList
-                memberId={selectedQuestionGroup.memberId}
+  return (
+    <>
+      <Container>
+        <TaskNavigationWrapper>
+          <TopBar>
+            <TopBarItem selected={true}>
+              Incoming questions
+              <div className="count">{groups.length}</div>
+            </TopBarItem>
+          </TopBar>
+          <FilterBar>
+            <FilterBarItem onClick={() => setShowFilters(true)}>
+              Filters
+            </FilterBarItem>
+          </FilterBar>
+          <ListContainer>
+            {groups.map((group) => {
+              const previewQuestion = group?.questions?.slice(-1)[0] ?? ''
+              const preview = getQuestionBody(previewQuestion)
+
+              return (
+                <ListItem
+                  key={group.id}
+                  onClick={() => setSelectedQuestionGroup(group)}
+                  selected={group.memberId === selectedQuestionGroup?.memberId}
+                >
+                  <span className="name">
+                    {(group?.firstName ?? '') + ' ' + (group?.lastName ?? '')}
+                  </span>
+                  <span className="preview">{preview.text}</span>
+                  <div className="options">
+                    {formatDistanceToNowStrict(
+                      parseISO(previewQuestion.timestamp),
+                      {
+                        addSuffix: true,
+                      },
+                    )}
+                  </div>
+                </ListItem>
+              )
+            })}
+          </ListContainer>
+        </TaskNavigationWrapper>
+        <TaskChatWrapper>
+          {selectedQuestionGroup && (
+            <>
+              <div
                 style={{
-                  padding: '2rem',
-                  marginBottom: '6rem',
-                  paddingBottom: '0',
+                  height: isLarge ? 'calc(100% - 27rem)' : 'calc(100% - 15rem)',
+                  transition: 'height 200ms',
                 }}
-              />
+              >
+                <MessagesList
+                  memberId={selectedQuestionGroup.memberId}
+                  style={{
+                    padding: '2rem',
+                    marginBottom: '6rem',
+                    paddingBottom: '0',
+                  }}
+                />
+              </div>
+              <div
+                style={{
+                  position: 'absolute',
+                  bottom: 0,
+                  left: 0,
+                  width: '100%',
+                  minHeight: '10rem',
+                }}
+              >
+                <TaskChatInput
+                  onResize={() => setIsLarge(!isLarge)}
+                  isLarge={isLarge}
+                  memberId={selectedQuestionGroup.memberId}
+                  onBlur={() => void 0}
+                  onFocus={() => void 0}
+                  onResolve={() =>
+                    setSelectedQuestionGroup(
+                      questionGroups.length !== 0 ? questionGroups[0] : null,
+                    )
+                  }
+                />
+              </div>
+            </>
+          )}
+        </TaskChatWrapper>
+      </Container>
+      {showFilters && (
+        <FilterModal onClose={() => setShowFilters(false)}>
+          <Flex
+            style={{ height: '100%', width: '100%' }}
+            direction="column"
+            justify="space-between"
+          >
+            <div>
+              <h4>Select Filters</h4>
+
+              <Flex
+                style={{
+                  flexWrap: 'wrap',
+                  marginTop: '4rem',
+                }}
+                justify="space-between"
+              >
+                <FilterSelect
+                  filters={selectedFilters}
+                  onToggle={toggleFilter}
+                  animationDelay={0}
+                  animationItemDelay={20}
+                />
+              </Flex>
             </div>
-            <div
-              style={{
-                position: 'absolute',
-                bottom: 0,
-                left: 0,
-                width: '100%',
-                minHeight: '10rem',
-              }}
-            >
-              <TaskChatInput
-                onResize={() => setIsLarge(!isLarge)}
-                isLarge={isLarge}
-                memberId={selectedQuestionGroup.memberId}
-                onBlur={() => void 0}
-                onFocus={() => void 0}
-                onResolve={() => void 0}
-              />
-            </div>
-          </>
-        )}
-      </TaskChatWrapper>
-    </Container>
+          </Flex>
+        </FilterModal>
+      )}
+    </>
   )
 }
 
