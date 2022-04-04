82e65ef6af48eec244216bdfd03982e951a31035
Maxim <maxim.pletenev@fastdev.se>
2022-04-04 15:47:02 +0500

    Add note creating to reserve

--- src/portals/hope/features/claims/claim-details/ClaimReserveForm.tsx
+++ src/portals/hope/features/claims/claim-details/ClaimReserveForm.tsx
@@ -3,21 +3,31 @@ import {
   ClaimPaymentsDocument,
   useUpdateReserveMutation,
 } from 'types/generated/graphql'
-
 import { Button, Input, Spacing } from '@hedvig-ui'
 import { PushUserAction } from 'portals/hope/features/tracking/utils/tags'
+import { useClaimNotes } from 'portals/hope/features/claims/claim-details/ClaimNotes'
 
 const isStringNumber = (s: string) => /^-?\d+$/.test(s) || /^\d+\.\d+$/.test(s)
 
 const ClaimReserveForm: React.FC<{ claimId: string }> = ({ claimId }) => {
   const [updateReserve, { loading }] = useUpdateReserveMutation()
   const [value, setValue] = useState('')
+  const [note, setNote] = useState('')
+
+  const { createNote } = useClaimNotes(claimId)
 
   return (
     <>
       <Input
+        value={value}
         placeholder="Reserve amount"
         onChange={(e) => setValue(e.currentTarget.value)}
+      />
+      <Spacing top="small" />
+      <Input
+        value={note}
+        placeholder="Reserve note"
+        onChange={(e) => setNote(e.currentTarget.value)}
       />
       <Spacing top="small" />
       <Button
@@ -35,8 +45,16 @@ const ClaimReserveForm: React.FC<{ claimId: string }> = ({ claimId }) => {
               { query: ClaimPaymentsDocument, variables: { claimId } },
             ],
           })
-          setValue('')
+
           PushUserAction('claim', 'update', 'reserve', null)
+
+          if (note) {
+            createNote(note)
+            PushUserAction('claim', 'add', 'note', null)
+          }
+
+          setValue('')
+          setNote('')
         }}
       >
         Update reserve
