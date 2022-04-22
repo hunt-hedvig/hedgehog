a21babad3737ea49748f36f2a67e799dab578021
rasmusguterstam <rasmus@hedvig.com>
2022-04-22 15:50:45 +0200

    Fix linting

--- src/portals/hope/features/tasks/TaskChat.tsx
+++ src/portals/hope/features/tasks/TaskChat.tsx
@@ -1,12 +1,12 @@
-import {ChatFill} from 'react-bootstrap-icons'
-import {Button, Flex, Placeholder} from '@hedvig-ui'
-import {MessagesList} from 'portals/hope/features/member/messages/MessagesList'
-import {TaskChatInput} from 'portals/hope/features/tasks/components/TaskChatInput'
-import React, {useState} from 'react'
+import { ChatFill } from 'react-bootstrap-icons'
+import { Button, Flex, Placeholder } from '@hedvig-ui'
+import { MessagesList } from 'portals/hope/features/member/messages/MessagesList'
+import { TaskChatInput } from 'portals/hope/features/tasks/components/TaskChatInput'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import chroma from 'chroma-js'
-import {useMemberHasOpenClaim} from 'portals/hope/common/hooks/use-member-has-open-claim'
-import {Task} from 'types/generated/graphql'
+import { useMemberHasOpenClaim } from 'portals/hope/common/hooks/use-member-has-open-claim'
+import { Task } from 'types/generated/graphql'
 
 const InChatTopNav = styled.div`
   cursor: pointer;
